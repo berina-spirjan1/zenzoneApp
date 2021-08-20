9fe8653f60034a3b892729791c9adffd8ab5a676
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-08-20 11:22:18 +0200

    Splash screen done.

--- screens/Splash.js
+++ screens/Splash.js
@@ -2,37 +2,29 @@ import React, {Component} from 'react';
 import {View} from "react-native";
 import LottieView from 'lottie-react-native';
 import ZenZoneTitle from "../components/ZenZoneTitle";
-import BackgroundThemeSplashScreen from "../components/BackgroundThemeSplashScreen";
 
-export default class Splash extends Component{
-    constructor(props) {
-        super();
-    }
-    render() {
-        return(
+function Splash(){
 
-           <View>
-                <LottieView
+    return(
+
+        <View>
+            <LottieView
                     style={styles.lottie}
                     source={require("../assets/images/men.json")}
-                    autoPlay
+                    autoPlay={true}
                     loop={true}/>
-               {/*todo change template for background in adobe xd*/}
-               {/*<BackgroundThemeSplashScreen/>*/}
-                <ZenZoneTitle style={styles.title}/>
+            {/*todo change template for background in adobe xd*/}
+            {/*<BackgroundThemeSplashScreen/>*/}
+            <ZenZoneTitle style={styles.title}/>
 
-            </View>
-        )
-    }
+        </View>
+    )
 }
-
+export default Splash;
 
 const styles={
     lottie:{
         flex:1,
-        width: 200,
-        height: 200,
-        marginLeft:25,
         alignItems: 'center',
         justifyContent: 'center',
     },
