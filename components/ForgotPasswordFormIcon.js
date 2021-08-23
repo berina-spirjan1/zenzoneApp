ce17468303508c8e259e5660f9d25b210fb6ce19
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-08-23 15:22:14 +0200

    Added icons for fogot password page.

---
+++ components/ForgotPasswordFormIcon.js
@@ -0,0 +1,26 @@
+import * as React from "react"
+import Svg, { Defs, G, Path } from "react-native-svg"
+
+
+function ForgotPasswordFormIcon(props) {
+    return (
+        <Svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={314.857}
+            height={491.729}
+            viewBox="0 0 314.857 491.729"
+            {...props}
+        >
+            <Defs></Defs>
+            <G filter="url(#prefix__a)">
+                <Path
+                    fill="#1c1f23"
+                    d="M9 6h296.857v473.729H9z"
+                    data-name="Rectangle 128"
+                />
+            </G>
+        </Svg>
+    )
+}
+
+export default ForgotPasswordFormIcon;
