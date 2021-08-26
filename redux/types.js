80beffe0a57b201fe0bd3c93c39da63fd5a5a125
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-08-26 10:45:21 +0200

    Redux implementation started.

---
+++ redux/types.js
@@ -0,0 +1,14 @@
+export const AUTH_SUCCESS = 'auth_success';
+export const AUTH_FAILED = 'auth_failed';
+
+
+export const USER_REGISTRATION_SUCCESS = 'user_registration_success';
+export const USER_REGISTRATION_FAILED = 'user_registration_failed';
+export const SUCCESSFULLY_ADDED_ACTIVITY = 'successfully_added_activity';
+export const FAILED_ADDING_ACTIVITY = 'failed_adding_activity';
+export const SENDING_REPORT_FOR_CHALLENGE_SUCCESS = 'sending_report_for_challenge_success';
+export const FAILED_AT_SENDING_REPORT_FOR_CHALLENGE = 'failed_at_sending_report_for_challenge';
+export const STARTED_UPDATING_USER_INFORMATIONS = 'started_updating_user_informations';
+export const FAILED_UPDATING_USER_INFORMATIONS = 'failed_updating_user_informations';
+
+//todo add more states for different status in pages!
