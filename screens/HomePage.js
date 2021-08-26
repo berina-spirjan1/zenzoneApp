90af48e1a3f44b0aa6116d3ecf83d4256d8f4788
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-08-26 18:24:38 +0200

    Toolbar for home page added.

--- screens/HomePage.js
+++ screens/HomePage.js
@@ -1,7 +1,11 @@
 import React, {Component} from "react";
-import {View} from "react-native";
+import {
+    View,
+    StyleSheet, StatusBar
+} from "react-native";
 import {createAppContainer} from "react-navigation";
 import {createDrawerNavigator} from "react-navigation-drawer";
+import {Toolbar} from "react-native-material-ui";
 
 export default class HomePage extends Component{
     constructor(props) {
@@ -10,19 +14,32 @@ export default class HomePage extends Component{
 
     render() {
         return(
-            <View>
+            <>
+                <StatusBar
+                    animated={true}
+                    backgroundColor="#6285B3"/>
+                <Toolbar style={{ container: {backgroundColor: '#93B4E5'}}}
+                    leftElement="menu"
+                    centerElement="Activities"
+                    searchable={{
+                        autoFocus: true,
+                        placeholder: 'Search',
+                    }}
+                    rightElement={{
+                        menu: {
+                            icon: "more-vert",
+                            labels: ["Most popular activities", "Daily challenge", "Leaderboard", "Theme"]
+                        }
+                    }}
+                    onRightElementPress={ (label) => { console.log(label) }}/>
+                <View>
 
-            </View>
+                </View>
+            </>
+
         )
     }
 }
-
-
-const DrawerNavigation=createDrawerNavigator({
-        //todo add menu items for drawer side menu
-},{
-    contentComponent: props => <SideBar {...props} />
-})
 
 
 const styleLightMode=StyleSheet.create({
@@ -31,7 +48,6 @@ const styleLightMode=StyleSheet.create({
     },
     bottomNavigationBar:{
         flex:1,
-        backgroundColor:"",
         justifyContent:'flex-end'
     },
 })
@@ -42,4 +58,3 @@ const styleDarkMode=StyleSheet.create({
     }
 })
 
-export default createAppContainer(DrawerNavigation);
