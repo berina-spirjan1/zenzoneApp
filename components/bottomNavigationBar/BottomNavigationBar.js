08c900f964d0335ae398b17c555088bd4106d496
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-08-29 14:13:17 +0200

    Bottom navigation bar - colors changed.

--- components/bottomNavigationBar/BottomNavigationBar.js
+++ components/bottomNavigationBar/BottomNavigationBar.js
@@ -147,7 +147,7 @@ const styles = StyleSheet.create({
         width:50,
         height:50,
         borderRadius:25,
-        backgroundColor: '#93B4E5'
+        backgroundColor: '#6285B3'
     },
     menuItem:{
         flex:1,
