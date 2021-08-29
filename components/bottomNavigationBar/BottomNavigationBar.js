ecb58f4ba2204adaf387c9a12690a9363fa44b42
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-08-29 19:38:22 +0200

    Coding standard updated for bottom navigation bar.

--- components/bottomNavigationBar/BottomNavigationBar.js
+++ components/bottomNavigationBar/BottomNavigationBar.js
@@ -4,10 +4,8 @@ import {
     createBottomTabNavigator
 } from "@react-navigation/bottom-tabs";
 import { FontAwesome5 } from "@expo/vector-icons";
-import Svg, { Path } from "react-native-svg";
 import {
     View,
-    TouchableOpacity,
     StyleSheet
 } from 'react-native';
 
@@ -17,72 +15,10 @@ import Leaderboard from "../../screens/Leaderboard";
 import SignUp from "../../screens/SignUp";
 import Login from "../../screens/Login";
 import {isIphoneX} from "react-native-iphone-x-helper";
+import TabBarCustomButton from "./TabBarCustomButton";
+import CustomTabBar from "./CustomTabBar";
 
 const Tab = createBottomTabNavigator();
-
-function TabBarCustomButton({accessibilityState, children, onPress}){
-    let isSelected = accessibilityState.selected;
-
-    if (isSelected){
-        return(
-            <View style={{ flex: 1,
-                           alignItems: 'center' }}>
-                <View style={{ flexDirection: 'row',
-                               position: 'absolute',
-                               top:0 }}>
-                    <View style={{ flex: 1,
-                                   backgroundColor: '#93B4E5' }}/>
-                    <Svg
-                        width={75}
-                        height={61}
-                        viewBox="0 0 75 61">
-                        <Path
-                            d={"M75.2 0v61H0V0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z"}
-                            fill={"#93B4E5"}/>
-                    </Svg>
-                    <View style={{flex: 1, backgroundColor: '#93B4E5'}}/>
-                </View>
-
-                <TouchableOpacity style={styles.menuItemButtonPressed}
-                                  activeOpacity={1}
-                                  onPress={onPress}>
-                    {children}
-                </TouchableOpacity>
-
-            </View>
-
-
-        )
-    }
-    else{
-        return(
-            <TouchableOpacity style={styles.menuItem}
-                              activeOpacity={1}
-                              onPress={onPress}>
-                {children}
-            </TouchableOpacity>
-        )
-    }
-
-}
-
-function CustomTabBar(props){
-    if (isIphoneX()){
-        return (
-            <View>
-                <View style={styles.customTabBar}/>
-                <BottomTabBar {...props.props}/>
-            </View>
-
-        )
-    }
-    else{
-        return(
-            <BottomTabBar {...props.props}/>
-            )
-    }
-}
-
 
 function BottomNavigationBar(){
     return(
@@ -164,30 +100,6 @@ function BottomNavigationBar(){
     )
 }
 
-const styles = StyleSheet.create({
-    menuItemButtonPressed:{
-        top: -22.5,
-        justifyContent: 'center',
-        alignItems: 'center',
-        width:50,
-        height:50,
-        borderRadius:25,
-        backgroundColor: '#6285B3'
-    },
-    menuItem:{
-        flex:1,
-        height: 50,
-        backgroundColor: '#93B4E5'
-    },
-    customTabBar:{
-        position: 'absolute',
-        bottom:0,
-        left:0,
-        right:0,
-        height: 30,
-        backgroundColor: 'white'
-    }
-})
 
 
 export default BottomNavigationBar;
