c36d9d3a7c81b44338dbffa16036f61ad90042d4
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-08-29 19:29:34 +0200

    Bottom navigation bar edited design in case the device is an iPhone.

--- components/bottomNavigationBar/BottomNavigationBar.js
+++ components/bottomNavigationBar/BottomNavigationBar.js
@@ -176,7 +176,7 @@ const styles = StyleSheet.create({
     },
     menuItem:{
         flex:1,
-        height: 60,
+        height: 50,
         backgroundColor: '#93B4E5'
     },
     customTabBar:{
