e62f8568f65a69d464990f5366808e08ffe2f75b
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-08-29 14:06:57 +0200

    Bottom navigation bar - updated selected item on menu.

--- components/bottomNavigationBar/BottomNavigationBar.js
+++ components/bottomNavigationBar/BottomNavigationBar.js
@@ -1,6 +1,13 @@
 import React from "react";
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-import {FontAwesome5} from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome5 } from "@expo/vector-icons";
+import Svg, { Path } from "react-native-svg";
+import {
+    View,
+    TouchableOpacity,
+    StyleSheet
+} from 'react-native';
+
 import HomePage from "../../screens/HomePage";
 import DailyChallengeDetails from "../../screens/DailyChallengeDetails";
 import Leaderboard from "../../screens/Leaderboard";
@@ -10,9 +17,63 @@ import Login from "../../screens/Login";
 
 const Tab = createBottomTabNavigator();
 
+function TabBarCustomButton({accessibilityState, children, onPress}){
+    let isSelected = accessibilityState.selected;
+
+    if (isSelected){
+        return(
+            <View style={{ flex: 1,
+                           alignItems: 'center' }}>
+                <View style={{ flexDirection: 'row',
+                               position: 'absolute',
+                               top:0 }}>
+                    <View style={{ flex: 1,
+                                   backgroundColor: '#93B4E5' }}/>
+                    <Svg
+                        width={75}
+                        height={61}
+                        viewBox="0 0 75 61">
+                        <Path
+                            d={"M75.2 0v61H0V0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z"}
+                            fill={"#93B4E5"}/>
+                    </Svg>
+                    <View style={{flex: 1, backgroundColor: '#93B4E5'}}/>
+                </View>
+
+                <TouchableOpacity style={styles.menuItemButtonPressed}
+                                  activeOpacity={1}
+                                  onPress={onPress}>
+                    {children}
+                </TouchableOpacity>
+
+            </View>
+
+
+        )
+    }
+    else{
+        return(
+            <TouchableOpacity style={styles.menuItem}
+                              activeOpacity={1}
+                              onPress={onPress}>
+                {children}
+            </TouchableOpacity>
+        )
+    }
+
+}
+
+
 function BottomNavigationBar(){
     return(
-        <Tab.Navigator>
+        <Tab.Navigator tabBarOptions={{
+            showLabel: false,
+            style:{
+                borderTopWidth: 0,
+                backgroundColor: 'transparent',
+                elevation: 0
+        }
+        }}>
             <Tab.Screen name={'Activities'}
                         component={HomePage}
                         options={{
@@ -20,6 +81,9 @@ function BottomNavigationBar(){
                                 <FontAwesome5 name={'book-reader'}
                                               size={focused ? 22 : 20}
                                               color={focused ? '#000' : '#616C75'}/>
+                            ),
+                            tabBarButton: (props) => (
+                                <TabBarCustomButton {...props}/>
                             )
                         }}/>
             <Tab.Screen name={'Challenge'}
@@ -29,6 +93,9 @@ function BottomNavigationBar(){
                                 <FontAwesome5 name={'clipboard-check'}
                                               size={focused ? 22 : 20}
                                               color={focused ? '#000' : '#616C75'}/>
+                            ),
+                            tabBarButton: (props) => (
+                                <TabBarCustomButton {...props}/>
                             )
                         }}/>
             <Tab.Screen name={'Ranking'}
@@ -38,6 +105,9 @@ function BottomNavigationBar(){
                                 <FontAwesome5 name={'medal'}
                                               size={focused ? 22 : 20}
                                               color={focused ? '#000' : '#616C75'}/>
+                            ),
+                            tabBarButton: (props) => (
+                                <TabBarCustomButton {...props}/>
                             )
                         }}/>
             <Tab.Screen name={'Login'}
@@ -47,8 +117,12 @@ function BottomNavigationBar(){
                                 <FontAwesome5 name={'user-check'}
                                               size={focused ? 22 : 20}
                                               color={focused ? '#000' : '#616C75'}/>
+                            ),
+                            tabBarButton: (props) => (
+                                <TabBarCustomButton {...props}/>
                             )
                         }}/>
+            {/*//todo add screen for creating new activity which will show in bottom navigation*/}
             <Tab.Screen name={'Sign Up'}
                         component={SignUp}
                         options={{
@@ -56,10 +130,31 @@ function BottomNavigationBar(){
                                 <FontAwesome5 name={'user'}
                                               size={focused ? 22 : 20}
                                               color={focused ? '#000' : '#616C75'}/>
+                            ),
+                            tabBarButton: (props) => (
+                                <TabBarCustomButton {...props}/>
                             )
                         }}/>
         </Tab.Navigator>
     )
 }
+
+const styles = StyleSheet.create({
+    menuItemButtonPressed:{
+        top: -22.5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width:50,
+        height:50,
+        borderRadius:25,
+        backgroundColor: '#93B4E5'
+    },
+    menuItem:{
+        flex:1,
+        height: 60,
+        backgroundColor: '#93B4E5'
+    }
+})
+
 
 export default BottomNavigationBar;
