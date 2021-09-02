41811964b2dd1e3ece6dfb020bc1de59decc9b64
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-02 16:44:42 +0200

    Cards added and title input.

--- screens/CreateNewActivity.js
+++ screens/CreateNewActivity.js
@@ -4,9 +4,10 @@ import {
     Text,
     ScrollView,
     StyleSheet,
-    SafeAreaView
+    SafeAreaView, StatusBar, TextInput
 } from "react-native";
 import CategoryCard from "../components/homePageComponents/cards/CategoryCard";
+import {Toolbar} from "react-native-material-ui";
 
 
 export default class CreateNewActivity extends Component{
@@ -16,21 +17,46 @@ export default class CreateNewActivity extends Component{
     render() {
         return(
             <View style={styles.container}>
-                <Text style={styles.chooseCategory}>choose category</Text>
-                    <SafeAreaView>
-                        <ScrollView horizontal
-                                    showsHorizontalScrollIndicator={false}>
-                            <View style={styles.categoryCard}>
-                                <CategoryCard/>
-                            </View>
-                            <View style={styles.categoryCard}>
-                                <CategoryCard/>
-                            </View>
-                            <View style={styles.categoryCard}>
-                                <CategoryCard/>
-                            </View>
-                        </ScrollView>
-                    </SafeAreaView>
+                <StatusBar
+                    animated={true}
+                    backgroundColor="#6285B3"/>
+                <Toolbar style={{ container: { backgroundColor: '#93B4E5' } }}
+                         // leftElement="arrow-back"
+                         centerElement="Activities"
+                         onRightElementPress={ (label) => { console.log(label) }}/>
+                <SafeAreaView>
+                             <ScrollView>
+                                 <View style={{flexDirection: 'row'}}>
+                                     <Text style={styles.chooseCategory}>choose category</Text>
+                                     <Text style={styles.seeAll}
+                                           onPress={(e) => this.onTextPress(e, 'See all')}>See all</Text>
+                                 </View>
+                                 <SafeAreaView>
+                                     <ScrollView horizontal
+                                                 showsHorizontalScrollIndicator={false}>
+                                         <View style={styles.categoryCard}>
+                                             <CategoryCard/>
+                                         </View>
+                                         <View style={styles.categoryCard}>
+                                             <CategoryCard/>
+                                         </View>
+                                         <View style={styles.categoryCard}>
+                                             <CategoryCard/>
+                                         </View>
+                                         <View style={styles.categoryCard}>
+                                             <CategoryCard/>
+                                         </View>
+                                         <View style={styles.categoryCard}>
+                                             <CategoryCard/>
+                                         </View>
+                                     </ScrollView>
+                                 </SafeAreaView>
+                                 <Text style={styles.title}>Title</Text>
+                                 <TextInput numberOfLines={2}
+                                            placeholder={'Activity title'}
+                                            style={styles.titleInput}/>
+                             </ScrollView>
+                </SafeAreaView>
 
             </View>
         )
@@ -40,15 +66,43 @@ export default class CreateNewActivity extends Component{
 const styles = StyleSheet.create({
     container:{
         fontFamily: 'Roboto_400Regular',
-        backgroundColor: '#CBDBF2'
+        backgroundColor: '#CBDBF2',
+        flex:1
     },
     chooseCategory:{
         fontWeight: 'bold',
         fontSize: 17,
         color: '#000000',
-        marginLeft: 20
+        marginLeft: 20,
+        marginTop: 30,
+        textTransform: 'uppercase'
     },
     categoryCard:{
-
+        height: 115,
+        width: 90,
+        marginTop: 10,
+        marginLeft: 17,
+        borderRadius: 50
+    },
+    seeAll:{
+        marginRight: 20,
+        marginTop: 30,
+        marginLeft: 120
+    },
+    title:{
+        textTransform: 'uppercase',
+        fontWeight: 'bold',
+        fontSize: 17,
+        marginLeft: 20,
+        marginTop:30
+    },
+    titleInput:{
+        backgroundColor: '#FFF',
+        opacity: 0.5,
+        borderRadius: 26,
+        width: 300,
+        marginLeft: 20,
+        marginTop: 10,
+        padding: 10
     }
 })
