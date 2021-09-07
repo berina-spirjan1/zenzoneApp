8d6fd3924d62ecc65b45826ae7ec0e918cfda634
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-07 16:16:38 +0200

    Page for leaderboard done.

--- screens/Leaderboard.js
+++ screens/Leaderboard.js
@@ -25,25 +25,34 @@ export default class Leaderboard extends Component{
         return (
           <View style={styles.container}>
               <ImageBackground source={require('../assets/images/backgroundLeaderboardLightMode.png')}
-                               style={styles.imageBackground}
-                               resizeMode={'cover'}>
+                               style={styles.imageBackground}/>
                   <FontAwesome5 name={'crown'}
-                                size={30}
+                                size={40}
                                 color={'#FFD700'}
                                 style={styles.crownIcon}/>
                   <View style={styles.rankings}>
                       <View style={styles.firstPlace}>
                           <Image source={require('../assets/images/rodjoImage.png')}
-                                 style={styles.userImagePlaces}/>
+                                 style={styles.userImageRankings}/>
+                          <Text style={styles.usernameFirstPlace}
+                                numberOfLines={2}>@rodjo</Text>
+                          <Text style={styles.badgesCounterFirstPlace}>15</Text>
                       </View>
                       <View style={styles.secondPlace}>
-
+                          <Image source={require('../assets/images/rodjoImage.png')}
+                                 style={styles.userImageRankings}/>
+                          <Text style={styles.usernameSecondPlace}
+                                numberOfLines={2}>@rodjo</Text>
+                          <Text style={styles.badgesCounterSecondPlace}>15</Text>
                       </View>
                       <View style={styles.thirdPlace}>
-
+                          <Image source={require('../assets/images/rodjoImage.png')}
+                                 style={styles.userImageRankings}/>
+                          <Text style={styles.usernameThirdPlace}
+                                numberOfLines={2}>@rodjo</Text>
+                          <Text style={styles.badgesCounterThirdPlace}>15</Text>
                       </View>
                   </View>
-              </ImageBackground>
               <SafeAreaView style={styles.safeArea}
                             style={{height: screenHeight}}>
                   <ScrollView vertical={true}
@@ -93,11 +102,9 @@ const styles = StyleSheet.create({
         marginTop: 20
     },
     safeArea:{
-        top: 300,
         flex:1,
         paddingTop: StatusBar.currentHeight,
-        paddingVertical: 20,
-        bottom:2007
+        paddingVertical: 20
     },
     scrollView:{
         alignSelf: 'stretch',
@@ -108,25 +115,95 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         marginLeft:-250,
         marginTop: -80,
+        flex: 1,
+        marginBottom:300
     },
     rankings:{
-
+        // bottom:270
     },
     firstPlace:{
-
+        width:100,
+        height:100,
+        shadowColor: '#334A6D',
+        shadowOffset: {
+            width: 10,
+            height: 5,
+        },
+        shadowOpacity: 0.8,
+        shadowRadius: 10,
+        elevation: 10,
+        marginLeft: 130,
+        marginTop: -140,
+        zIndex:2
     },
     secondPlace:{
-
+        width:100,
+        height:100,
+        marginLeft:30,
+        marginTop:-60,
+        shadowColor: '#334A6D',
+        shadowOffset: {
+            width: 10,
+            height: 5,
+        },
+        shadowOpacity: 0.8,
+        shadowRadius: 10,
+        elevation: 10,
+        marginBottom: 140
     },
     thirdPlace:{
-
-    },
-    userImagePlaces:{
         width:100,
-        height: 100,
-        borderRadius:100
+        height:100,
+        marginLeft:230,
+        marginTop:-220,
+        shadowColor: '#334A6D',
+        shadowOffset: {
+            width: 10,
+            height: 5,
+        },
+        shadowOpacity: 0.8,
+        shadowRadius: 10,
+        elevation: 10,
+        marginBottom: 140
     },
     crownIcon:{
-
+        top:-150,
+        left:155
+    },
+    userImageRankings:{
+        height: 100,
+        width:100,
+        borderRadius:100,
+    },
+    usernameFirstPlace:{
+        marginTop:5,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color: '#000000'
+    },
+    badgesCounterFirstPlace:{
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color: '#334A6D'
+    },
+    usernameSecondPlace:{
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginTop:5
+    },
+    badgesCounterSecondPlace:{
+        textAlign: 'center',
+        color:'#334A6D',
+        fontWeight: 'bold'
+    },
+    usernameThirdPlace:{
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginTop:5
+    },
+    badgesCounterThirdPlace:{
+        textAlign: 'center',
+        color:'#334A6D',
+        fontWeight: 'bold'
     }
 })
