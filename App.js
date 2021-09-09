8c01b86808e90981779595414a73a7b008e0f16a
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-09 23:11:40 +0200

    Implemented routes for profile page.

--- App.js
+++ App.js
@@ -4,17 +4,7 @@ import {NavigationContainer} from "@react-navigation/native";
 import BottomNavigationBar from "./components/bottomNavigationBar/BottomNavigationBar";
 import {createStackNavigator} from "@react-navigation/stack";
 import AppLoading from 'expo-app-loading';
-import {
-    useFonts,
-    Roboto_400Regular,
-    Roboto_100Thin_Italic,
-    Roboto_700Bold_Italic
-} from '@expo-google-fonts/roboto';
-import {Router} from "react-native-router-flux";
-import RoutesLoginSignUp from "./routes/RoutesLoginSignUp";
-import UserProfile from "./screens/UserProfile";
-import MyProfile from "./screens/MyProfile";
-import Login from "./screens/Login";
+import {Roboto_100Thin_Italic, Roboto_400Regular, Roboto_700Bold_Italic, useFonts} from '@expo-google-fonts/roboto';
 
 const Stack = createStackNavigator();
 
