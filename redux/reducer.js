82925488a0087b32cf09f41642132a1d44c26df2
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-11 00:17:48 +0200

    Added new action types for redux.

--- redux/reducer.js
+++ redux/reducer.js
@@ -12,21 +12,35 @@ const reducer = (state = initialState, action) =>{
             return state;
         case Types.AUTH_SUCCESS:
             return state;
-        case Types.FAILED_ADDING_ACTIVITY:
-            return state;
-        case Types.FAILED_AT_SENDING_REPORT_FOR_CHALLENGE:
-            return state;
-        case Types.FAILED_UPDATING_USER_INFORMATIONS:
-            return state;
-        case Types.USER_REGISTRATION_SUCCESS:
+        case Types.USER_REGISTRATION_STARTED:
             return state;
         case Types.USER_REGISTRATION_FAILED:
             return state;
-        case Types.STARTED_UPDATING_USER_INFORMATIONS:
-            return state;
-        case Types.SENDING_REPORT_FOR_CHALLENGE_SUCCESS:
+        case Types.USER_REGISTRATION_SUCCESS:
             return state;
         case Types.SUCCESSFULLY_ADDED_ACTIVITY:
+            return state;
+        case Types.FAILED_ADDING_ACTIVITY:
+            return state;
+        case Types.STARTED_ADDING_ACTIVITY:
+            return state;
+        case Types.STARTED_UPDATING_ACTIVITY:
+            return state;
+        case Types.FAILED_UPDATING_ACTIVITY:
+            return state;
+        case Types.SUCCESSFULLY_UPDATED_ACTIVITY:
+            return state;
+        case Types.STARTED_DELETING_ACTIVITY:
+            return state;
+        case Types.FAILED_DELETING_ACTIVITY:
+            return state;
+        case Types.SUCCESSFULLY_DELETED_ACTIVITY:
+            return state;
+        case Types.STARTED_SENDING_REPORT_FOR_CHALLENGE :
+            return state;
+        case Types.FAILED_AT_SENDING_REPORT_FOR_CHALLENGE:
+            return state;
+        case Types.SENDING_REPORT_FOR_CHALLENGE_SUCCESS:
             return state;
         default:
             return state;
--- redux/types.js
+++ redux/types.js
@@ -1,14 +1,36 @@
+
+//login
 export const AUTH_STARTED = 'auth_started';
 export const AUTH_SUCCESS = 'auth_success';
 export const AUTH_FAILED = 'auth_failed';
 
-
-export const USER_REGISTRATION_SUCCESS = 'user_registration_success';
+//registration
+export const USER_REGISTRATION_STARTED = 'user_registration_started';
 export const USER_REGISTRATION_FAILED = 'user_registration_failed';
+export const USER_REGISTRATION_SUCCESS = 'user_registration_success';
+
+//adding activity
 export const SUCCESSFULLY_ADDED_ACTIVITY = 'successfully_added_activity';
 export const FAILED_ADDING_ACTIVITY = 'failed_adding_activity';
-export const SENDING_REPORT_FOR_CHALLENGE_SUCCESS = 'sending_report_for_challenge_success';
+export const STARTED_ADDING_ACTIVITY = 'started_adding_activity';
+
+//updating activity
+export const STARTED_UPDATING_ACTIVITY = 'started_updating_activity';
+export const FAILED_UPDATING_ACTIVITY = 'failed_updating_activity';
+export const SUCCESSFULLY_UPDATED_ACTIVITY = 'successfully_updated_activity';
+
+//deleting activity
+export const STARTED_DELETING_ACTIVITY = 'started_deleting_activity';
+export const FAILED_DELETING_ACTIVITY = 'failed_deleting_activity';
+export const SUCCESSFULLY_DELETED_ACTIVITY = 'successfully_deleted_activity';
+
+//sending report for daily challenge
+export const STARTED_SENDING_REPORT_FOR_CHALLENGE = 'started_sending_report_for_challenge';
 export const FAILED_AT_SENDING_REPORT_FOR_CHALLENGE = 'failed_at_sending_report_for_challenge';
+export const SENDING_REPORT_FOR_CHALLENGE_SUCCESS = 'sending_report_for_challenge_success';
+
+
+
 export const STARTED_UPDATING_USER_INFORMATIONS = 'started_updating_user_informations';
 export const FAILED_UPDATING_USER_INFORMATIONS = 'failed_updating_user_informations';
 
