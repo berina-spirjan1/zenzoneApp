0c5e2769cabf6fd9324a75ffeef831c145c8f666
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-11 19:42:52 +0200

    Implemented routes for leaderboard page.

---
+++ routes/RoutesLeaderboardUser.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import {Router, Scene, Stack} from "react-native-router-flux";
+import UserProfile from "../screens/userProfile/UserProfile";
+import Leaderboard from "../screens/Leaderboard";
+
+export default class RoutesLeaderboardUser extends Component{
+    render() {
+        return(
+            <Router>
+                <Stack key={"root"} hideNavBar={true}>
+                    <Scene key={"leaderboard"} component={Leaderboard} title={"Leaderboard"} initial={true}/>
+                    <Scene key={"userProfile"} component={UserProfile} title={"User profile"}/>
+                </Stack>
+            </Router>
+        )
+    }
+}
