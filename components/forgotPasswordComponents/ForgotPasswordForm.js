354256f340cf35f70e679c36d09564e94210611d
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-12 18:01:50 +0200

    Implemented linking via the post method on api for changing password and created a new component in which the form for a given page is located.

---
+++ components/forgotPasswordComponents/ForgotPasswordForm.js
@@ -0,0 +1,129 @@
+import {Alert, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
+import {FontAwesome5} from "@expo/vector-icons";
+import ForgotPasswordFormIcon from "../../assets/icons/ForgotPasswordFormIcon";
+import ForgotPasswordWomenIcon from "../../assets/icons/ForgotPasswordWomenIcon";
+import React, {useState} from "react";
+import {FORGOT_PASSWORD, REGISTER} from "../../configuration/config";
+import store from "../../redux/store";
+import {
+    forgotPasswordChangedSuccess,
+    forgotPasswordChangingFailed,
+    forgotPasswordChangingStarted,
+} from "../../redux/actions";
+
+
+export const ForgotPasswordForm = () =>{
+
+    const [email, setEmail] = useState('');
+
+    const onResetHandler = () =>{
+
+        const user = {
+            email,
+        };
+
+        fetch(`${FORGOT_PASSWORD}`,{
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+            body: JSON.stringify(user)
+        })
+            .then(async res => {
+                try{
+                    store.dispatch(forgotPasswordChangingStarted());
+
+                    const jsonRes = await res.json();
+
+                    console.log(jsonRes.message)
+                    if(res.status!==200){
+                        store.dispatch(forgotPasswordChangingFailed());
+                    }
+                    else{
+                        store.dispatch(forgotPasswordChangedSuccess());
+                    }
+                }
+                catch (err){
+                    console.log(err);
+                }
+            })
+    };
+
+    return(
+        <View style={ styles.container }>
+
+            <StatusBar
+                animated={true}
+                backgroundColor="#6285B3"/>
+            <TextInput style={ styles.inputEmail }
+                       onChangeText={setEmail}>
+                {/*<FontAwesome5 name={'envelope'}*/}
+                {/*              size={18}*/}
+                {/*              color={'#000000'}/>*/}
+            </TextInput>
+            <TouchableOpacity style={ styles.button }
+                              onPress={onResetHandler}>
+                <Text style={ styles.resetPassword }>Reset password</Text>
+            </TouchableOpacity>
+            <ForgotPasswordFormIcon style={{ top: -290}}>
+            </ForgotPasswordFormIcon>
+
+            <ForgotPasswordWomenIcon style={ styles.womenIcon }/>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: '#646668'
+    },
+    toolBar:{
+        backgroundColor: '#000000'
+    },
+    email:{
+        color: '#FFFFFF',
+        top: 320,
+    },
+    inputEmail:{
+        height: 50,
+        width: 200,
+        margin: 12,
+        padding: 10,
+        marginLeft: 45,
+        marginRight: 30,
+        borderRadius: 27,
+        marginTop: 130,
+        backgroundColor: '#FFFFFF',
+        opacity: 0.5,
+        color: '#000000',
+        borderWidth: 0,
+        shadowColor: '#000000',
+        elevation: 5,
+        shadowOffset: { width: 20, height: 2 },
+        shadowOpacity: 1,
+        shadowRadius: 10,
+        top: 140,
+        zIndex: 2
+    },
+    button:{
+        backgroundColor: '#6285B3',
+        height: 40,
+        top: 150,
+        width: 150,
+        borderRadius: 27,
+        marginLeft: 70,
+        zIndex: 2
+    },
+    resetPassword:{
+        textAlign: 'center',
+        justifyContent: 'center',
+        fontWeight: 'bold',
+        padding:12,
+        textTransform: 'uppercase'
+    },
+    womenIcon:{
+        top: -360
+    }
+})
--- screens/ForgotPassword.js
+++ screens/ForgotPassword.js
@@ -1,19 +1,10 @@
 import React, { Component } from "react";
-import {
-    View,
-    Text,
-    TextInput,
-    StyleSheet,
-    StatusBar,
-    TouchableOpacity,
-    Alert
-} from "react-native";
-import { FontAwesome5 } from "@expo/vector-icons";
-
-
-import ForgotPasswordFormIcon from "../assets/icons/ForgotPasswordFormIcon";
-import ForgotPasswordWomenIcon from "../assets/icons/ForgotPasswordWomenIcon";
 import { Toolbar } from "react-native-material-ui";
+import { Provider } from "react-redux";
+
+import { ForgotPasswordForm } from "../components/forgotPasswordComponents/ForgotPasswordForm";
+import store from "../redux/store";
+
 
 export default class ForgotPassword extends Component{
     constructor(props) {
@@ -22,82 +13,11 @@ export default class ForgotPassword extends Component{
 
     render() {
         return(
-            <>
+            <Provider store={store}>
                 <Toolbar style={{ container: { backgroundColor: '#93B4E5' }}}
                          centerElement=" Forgot password"/>
-                <View style={ styles.container }>
-
-                    <StatusBar
-                        animated={true}
-                        backgroundColor="#6285B3"/>
-                    <TextInput style={ styles.inputEmail }>
-                        <FontAwesome5 name={'envelope'}
-                                      size={18}
-                                      color={'#000000'}/>
-                    </TextInput>
-                    <TouchableOpacity style={ styles.button }
-                                      onPress={() => Alert.alert('Button clicked')}>
-                        <Text style={ styles.resetPassword }>Reset password</Text>
-                    </TouchableOpacity>
-                    <ForgotPasswordFormIcon style={{ top: -290}}>
-                    </ForgotPasswordFormIcon>
-
-                    <ForgotPasswordWomenIcon style={ styles.womenIcon }/>
-                </View>
-        </>
+                <ForgotPasswordForm/>
+            </Provider>
         )
-    }
+                }
 }
-
-const styles = StyleSheet.create({
-    container:{
-        backgroundColor: '#646668'
-    },
-    toolBar:{
-        backgroundColor: '#000000'
-    },
-    email:{
-        color: '#FFFFFF',
-        top: 320,
-    },
-    inputEmail:{
-        height: 50,
-        width: 200,
-        margin: 12,
-        padding: 10,
-        marginLeft: 45,
-        marginRight: 30,
-        borderRadius: 27,
-        marginTop: 130,
-        backgroundColor: '#FFFFFF',
-        opacity: 0.5,
-        color: '#000000',
-        borderWidth: 0,
-        shadowColor: '#000000',
-        elevation: 5,
-        shadowOffset: { width: 20, height: 2 },
-        shadowOpacity: 1,
-        shadowRadius: 10,
-        top: 140,
-        zIndex: 2
-    },
-    button:{
-        backgroundColor: '#6285B3',
-        height: 40,
-        top: 150,
-        width: 150,
-        borderRadius: 27,
-        marginLeft: 70,
-        zIndex: 2
-    },
-    resetPassword:{
-        textAlign: 'center',
-        justifyContent: 'center',
-        fontWeight: 'bold',
-        padding:12,
-        textTransform: 'uppercase'
-    },
-    womenIcon:{
-        top: -360
-    }
-})
