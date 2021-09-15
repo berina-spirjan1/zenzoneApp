e24ca168fe0e217d7f39acaca92f7111c21002f0
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-15 14:41:33 +0200

    Created page for successfully done daily challenge.

---
+++ screens/Congratulations.js
@@ -0,0 +1,30 @@
+import React, {Component} from "react";
+import {
+    View,
+    StyleSheet
+} from "react-native";
+import LottieView from 'lottie-react-native';
+
+
+export default class Congratulations extends Component{
+
+
+    render() {
+        return(
+            <View style={styles.container}>
+                {/*<LottieView*/}
+                {/*    style={styles.lottie}*/}
+                {/*    source={require("../assets/images/men.json")}*/}
+                {/*    autoPlay={true}*/}
+                {/*    loop={true}/>*/}
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: '#93B4E5',
+        flex: 1
+    }
+})
