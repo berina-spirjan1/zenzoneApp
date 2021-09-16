091c470bc955e6aab1874c53b8c5223cb542e36d
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-16 10:08:10 +0200

    Created page for successfully added activity.

---
+++ screens/SuccessfullyAddedActivity.js
@@ -0,0 +1,23 @@
+import React from "react";
+import{
+    View,
+    StyleSheet
+} from "react-native"
+import LottieView from "lottie-react-native";
+
+function SuccessfullyAddedActivity(){
+    return(
+        <View style={styles.container}>
+
+        </View>
+    )
+}
+
+export default SuccessfullyAddedActivity;
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: '#93B4E5',
+        flex: 1
+    }
+})
