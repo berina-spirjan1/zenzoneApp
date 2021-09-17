21e4b798b455603a0fe7dd371c149bf934a151cc
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-17 09:49:49 +0200

    Added new route for challenge.

--- routes/RoutesForChallenge.js
+++ routes/RoutesForChallenge.js
@@ -3,6 +3,7 @@ import {Router, Scene, Stack} from "react-native-router-flux";
 import DailyChallengeDetails from "../screens/DailyChallengeDetails";
 import DoDailyChallenge from "../screens/DoDailyChallenge";
 import Congratulations from "../screens/Congratulations";
+import DailyChallengeCounter from "../screens/DailyChallengeCounter";
 
 
 
@@ -14,6 +15,7 @@ export default class RoutesForChallenge extends Component{
                     <Scene key={"challengeDetails"} component={DailyChallengeDetails} title={"challengeDetails"} initial={true}/>
                     <Scene key={"doDaily"} component={DoDailyChallenge} title={"doDaily"}/>
                     <Scene key={"congratulations"} component={Congratulations} title={"congratulations"}/>
+                    <Scene key={"waiting"} component={DailyChallengeCounter} title={"waiting"}/>
                 </Stack>
             </Router>
         )
