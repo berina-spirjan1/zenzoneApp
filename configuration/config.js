5ad0a3fc1cafe1c5a3a1835f4f756696064936f2
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-18 20:01:25 +0200

    Added new endpoints for api.

--- configuration/config.js
+++ configuration/config.js
@@ -1,17 +1,22 @@
 export const API_URL = 'https://vucko-1049-backend.zendev.se/api';
 
+//authentication
 export const REGISTER =`${API_URL}/register`;
 export const LOGIN = `${API_URL}/login`;
 export const LOGOUT = `${API_URL}/logout`;
 
+//user
+export const USER = `${API_URL}/user`;
+export const USER_UPDATE = `${USER}/update`;
+
+//activity
+export const ACTIVITY = `${API_URL}/activity`;
+export const ACTIVITY_PUBLIC = `${ACTIVITY}/public`
 
 export const CHANGE_PASSWORD = `${API_URL}/change-password`;
 export const FORGOT_PASSWORD = `${API_URL}/forget-password`;
 
-export const ACTIVITY = `${API_URL}/activity`;
+
 export const CATEGORY = `${API_URL}/category`;
 
 
-
-
-export const USER = `${API_URL}/user`;
