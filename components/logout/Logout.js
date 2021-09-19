aabe318bd178da672fdc8d4f35b47c860278d89a
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-19 15:52:50 +0200

    Implemented function for user logout, because we need this function on more places.

---
+++ components/logout/Logout.js
@@ -0,0 +1,40 @@
+import {LOGOUT} from "../../configuration/config";
+import store from "../../redux/store";
+import {userLogoutFailed, userLogoutStarted, userLogoutSuccess} from "../../redux/actions";
+import {AsyncStorage} from "react-native";
+
+export const onLogoutHandler = async () => {
+
+    const token = await AsyncStorage.getItem('jwt')
+
+    fetch(`${LOGOUT}`, {
+        method: 'POST',
+        mode: 'no-cors',
+        cache: 'default',
+        credentials: 'same-origin',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            'Authorization': 'Bearer ' + token
+        },
+    })
+        .then(async res => {
+            try {
+                store.dispatch(userLogoutStarted());
+
+                const jsonRes = await res.json();
+
+                console.log(jsonRes)
+                if (res.status !== 200) {
+                    store.dispatch(userLogoutFailed());
+                    console.log("greska", res.status)
+                } else {
+                    await AsyncStorage.removeItem('jwt')
+                    console.log("Successfully logout")
+                    store.dispatch(userLogoutSuccess());
+                }
+            } catch (err) {
+                console.log(err);
+            }
+        })
+};
