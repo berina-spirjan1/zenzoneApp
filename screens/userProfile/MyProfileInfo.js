5be425eef24e790d2409413ae48e262b3a34a991
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-19 17:13:41 +0200

    Finished implementation for user info, missing only user photo.

--- screens/userProfile/MyProfileInfo.js
+++ screens/userProfile/MyProfileInfo.js
@@ -1,17 +1,62 @@
 import React, { Component } from "react";
 import {
     View,
-    StyleSheet, StatusBar, ImageBackground, Image, Text, SafeAreaView, ScrollView, TouchableOpacity
+    StyleSheet,
+    StatusBar,
+    ImageBackground,
+    Image,
+    Text,
+    SafeAreaView,
+    ScrollView,
+    TouchableOpacity,
+    Dimensions,
+    AsyncStorage
 } from "react-native";
 import { Toolbar } from "react-native-material-ui";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+
+import {onLogoutHandler} from "../../components/logout/Logout";
+import {USER} from "../../configuration/config";
 
 export default class MyProfileInfo extends Component{
     constructor(props) {
         super();
     }
+    state = {
+        data: ''
+    }
+
+    componentDidMount = async () => {
+
+        let token = await AsyncStorage.getItem('jwt')
+        token = JSON.parse(token)
+
+        fetch(`${USER}`, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                'Authorization': 'Bearer ' + token
+            }
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson);
+                this.setState({
+                    data: responseJson
+                })
+                // console.log(this.state.data)
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
 
     render(){
+
+        const screenHeight = Dimensions.get('window').height
+
         return(
             <View style={styles.container}>
                 <StatusBar
@@ -30,17 +75,26 @@ export default class MyProfileInfo extends Component{
                                  style={styles.imageBackground}/>
                 <Image source={require('../../assets/images/rodjoImage.png')}
                        style={styles.userImage}/>
-                <Text style={styles.username}>@arnel_maric</Text>
-                <SafeAreaView>
+                <Text style={styles.username}>{this.state.data.name}</Text>
+                <View style={styles.followersFollowing}>
+                    <Text style={styles.followers}>Followers</Text>
+                    <Text style={styles.following}>Following</Text>
+                </View>
+                <View style={styles.counters}>
+                    <Text style={styles.counterFollowers}>{this.state.data.followers_counter}</Text>
+                    <Text style={styles.following}>{this.state.data.following_counter}</Text>
+                </View>
+                <SafeAreaView style={styles.safeArea}
+                              style={{height: screenHeight}}>
                     <ScrollView vertical={true}
-                                style={{marginTop:0}}>
+                                style={styles.scrollView}>
                         <TouchableOpacity style={styles.items}>
                             <View style={styles.itemRow}>
                                 <FontAwesome5 name={'signature'}
-                                              size={20}
+                                              size={16}
                                               color={'#616C75'}
                                               style={styles.icon}/>
-                                <Text style={styles.menuItem}>username</Text>
+                                <Text style={styles.menuItem}>{this.state.data.name}</Text>
                             </View>
                         </TouchableOpacity>
                         <TouchableOpacity style={styles.items}>
@@ -49,7 +103,7 @@ export default class MyProfileInfo extends Component{
                                               size={20}
                                               color={'#616C75'}
                                               style={styles.icon}/>
-                                <Text style={styles.menuItem}>first name</Text>
+                                <Text style={styles.menuItem}>{this.state.data.first_name}</Text>
                             </View>
                         </TouchableOpacity>
                         <TouchableOpacity style={styles.items}>
@@ -58,7 +112,7 @@ export default class MyProfileInfo extends Component{
                                               size={20}
                                               color={'#616C75'}
                                               style={styles.icon}/>
-                                <Text style={styles.menuItem}>last name</Text>
+                                <Text style={styles.menuItem}>{this.state.data.last_name}</Text>
                             </View>
                         </TouchableOpacity>
                         <TouchableOpacity style={styles.items}>
@@ -67,7 +121,7 @@ export default class MyProfileInfo extends Component{
                                               size={20}
                                               color={'#616C75'}
                                               style={styles.icon}/>
-                                <Text style={styles.menuItem}>e-mail adress</Text>
+                                <Text style={styles.menuItem}>{this.state.data.email}</Text>
                             </View>
                         </TouchableOpacity>
                         <TouchableOpacity style={styles.items}>
@@ -75,7 +129,7 @@ export default class MyProfileInfo extends Component{
                                 <FontAwesome5 name={'map-marker-alt'}
                                               size={20}
                                               color={'#616C75'}/>
-                                <Text style={styles.menuItem}>office location</Text>
+                                <Text style={styles.menuItem}>{this.state.data.office_location}</Text>
                             </View>
                         </TouchableOpacity>
                         <TouchableOpacity style={styles.items}>
@@ -83,18 +137,21 @@ export default class MyProfileInfo extends Component{
                                 <FontAwesome5 name={'briefcase'}
                                               size={20}
                                               color={'#616C75'}/>
-                                <Text style={styles.menuItem}>work position</Text>
+                                <Text style={styles.menuItem}>{this.state.data.work_position}</Text>
                             </View>
                         </TouchableOpacity>
-                        <TouchableOpacity style={styles.bottomTabBar}>
+                        <TouchableOpacity style={styles.bottomTabBar}
+                                          onPress={async () => {
+                                              await onLogoutHandler()
+                                          }}>
                             <View style={styles.itemRow}>
                                 <FontAwesome5 name={'lock'}
                                               size={20}
                                               color={'#3B495E'}/>
                                 <Text style={styles.menuItem}>Change password</Text>
                             </View>
-
                         </TouchableOpacity>
+                        <View style={{marginBottom:100}}/>
                     </ScrollView>
                 </SafeAreaView>
 
@@ -113,48 +170,96 @@ const styles = StyleSheet.create({
         width: 700,
         justifyContent: 'center',
         marginLeft:-170,
-        marginTop: -90,
+        marginTop: -100,
         opacity:0.5,
         flex: 1,
         marginBottom:300
     },
     userImage:{
-    width: 150,
+        width: 150,
         height: 150,
         borderRadius: 200,
-        top: -80,
+        top: -140,
         left:110,
         textAlign: 'center'
     },
     username:{
         fontSize: 22,
-            top:-70,
-            fontFamily: 'Roboto_700Bold_Italic',
-            textAlign: 'center',
-            color:'#505760'
+        top:-130,
+        fontFamily: 'Roboto_700Bold_Italic',
+        textAlign: 'center',
+        color:'#505760'
     },
     items:{
         borderWidth: 0,
-            backgroundColor:'#CBDBF2'
+        backgroundColor:'#CBDBF2'
     },
     itemRow:{
         top: 10,
-            flexDirection: 'row',
-            paddingLeft: 80,
-            height: 60,
+        flexDirection: 'row',
+        paddingLeft: 80,
+        height: 60,
 
     },
     menuItem:{
         marginTop:2,
-            marginLeft: 45,
-            fontFamily: 'Roboto_700Bold_Italic',
-            fontWeight: 'bold',
-            color: '#616C75'
+        marginLeft: 45,
+        fontFamily: 'Roboto_700Bold_Italic',
+        fontWeight: 'bold',
+        color: '#616C75'
     },
     bottomTabBar:{
         backgroundColor: '#93B4E5',
-            height: 60,
-            paddingLeft: 0,
-            paddingTop:10
+        height: 60,
+        paddingLeft: 0,
+        paddingTop:10
+    },
+    safeArea:{
+        flex:1,
+        paddingTop: StatusBar.currentHeight,
+        paddingVertical: 20
+    },
+    scrollView:{
+        alignSelf: 'stretch',
+        marginTop: -10,
+        marginBottom: 425
+    },
+    followersFollowing:{
+        flexDirection: 'row',
+        marginTop: -110,
+        marginBottom: 100
+    },
+    followers:{
+        marginLeft: 40,
+        marginRight:140,
+        fontSize: 16,
+        fontFamily: 'Roboto_700Bold_Italic',
+        fontWeight: 'bold',
+        color:'#505760'
+    },
+    following: {
+        fontSize: 16,
+        fontFamily: 'Roboto_700Bold_Italic',
+        fontWeight: 'bold',
+        color:'#505760'
+    },
+    counters:{
+        flexDirection: 'row',
+        marginTop: -100,
+        marginBottom: 75
+    },
+    counterFollowers:{
+        marginLeft: 70,
+        marginRight: 200,
+        fontSize: 16,
+        fontFamily: 'Roboto_700Bold_Italic',
+        fontWeight: 'bold',
+        color:'#505760'
+    },
+    counterFollowing:{
+        fontSize: 16,
+        fontFamily: 'Roboto_700Bold_Italic',
+        fontWeight: 'bold',
+        color:'#505760'
     }
 })
