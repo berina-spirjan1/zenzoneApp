c19845b832679ec477570acbe75f056874f3d9ff
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-20 14:51:00 +0200

    Added function with get method for list of all activities.

--- components/activityComponents/SingleActivityGetInfo.js
+++ components/activityComponents/SingleActivityGetInfo.js
@@ -1,74 +1,94 @@
-import React from "react";
-import {Dimensions, Image, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View} from "react-native";
-import {Toolbar} from "react-native-material-ui";
-import {ACTIVITY_PUBLIC} from "../../configuration/config";
+import React, {Component} from "react";
+import {
+    AsyncStorage,
+    Dimensions,
+    Image,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    View
+} from "react-native";
+import { ACTIVITY } from "../../configuration/config";
 
 
-export const SingleActivityGetInfo = () =>{
-    const screenHeight = Dimensions.get('window').height
+export default class SingleActivityGetInfo extends Component{
 
-    // const getSingleActivityInfo = () =>{
-    //     fetch(`${ACTIVITY_PUBLIC}`, {
-    //         method: 'GET',
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //             "Accept": "application/json",
-    //         },
-    //     })
-    //         .then((response) => response.json())
-    //         //If response is in json then in success
-    //         .then((responseJson) => {
-    //             //Success
-    //             alert(JSON.stringify(responseJson));
-    //             console.log(responseJson);
-    //         })
-    //         //If response is not in json then in error
-    //         .catch((error) => {
-    //             //Error
-    //             alert(JSON.stringify(error));
-    //             console.error(error);
-    //         });
-    // }
+    state = {
+        data: '',
+    }
 
 
-    return(
-        <View style={styles.container}>
-            <SafeAreaView style={styles.safeArea}
-                          style={{height: screenHeight}}>
-                <ScrollView vertical={true}
-                            style={styles.scrollView}>
-                    <Image source={require('../assets/images/taj_mahal.jpg')}
-                           style={styles.image}/>
-                    <Text style={styles.activityTitle}>Activity title</Text>
-                    <Text style={styles.description}> Lorem ipsum dolor sit amet,
-                        consectetur adipiscing elit.
-                        Vestibulum iaculis tortor odio, a
-                        rutrum tortor faucibus cursus.
-                        Nulla fringilla congue magna vitae
-                        fermentum. Quisque eleifend arcu
-                        non cursus laoreet. Duis efficitur
-                        mattis eros convallis placerat.
-                        Maecenas lorem dui, gravida at
-                        odio eget, semper molestie leo.
-                        Pellentesque rhoncus facilisis mi
-                        semper ultricies.
-                        Lorem ipsum dolor sit amet,
-                        consectetur adipiscing elit.
-                        Vestibulum iaculis tortor odio, a
-                        rutrum tortor faucibus cursus.
-                        Nulla fringilla congue magna vitae
-                        fermentum. Quisque eleifend arcu
-                        non cursus laoreet. Duis efficitur
-                        mattis eros convallis placerat.
-                        Maecenas lorem dui, gravida at
-                        odio eget, semper molestie leo.
-                        Pellentesque rhoncus facilisis mi
-                        semper ultricies.</Text>
-                    <View style={{marginBottom: 100}}/>
-                </ScrollView>
-            </SafeAreaView>
-        </View>
-    )
+    componentDidMount = async () => {
+
+        let token = await AsyncStorage.getItem('jwt')
+        token = JSON.parse(token)
+
+        fetch(`${ACTIVITY}`, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                'Authorization': 'Bearer ' + token
+            }
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson);
+                this.setState({
+                    data: responseJson
+                })
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    render() {
+
+        const screenHeight = Dimensions.get('window').height
+
+        return(
+            <View style={styles.container}>
+                <SafeAreaView style={styles.safeArea}
+                              style={{height: screenHeight}}>
+                    <ScrollView vertical={true}
+                                style={styles.scrollView}>
+                        <Image source={require('../assets/images/taj_mahal.jpg')}
+                               style={styles.image}/>
+                        <Text style={styles.activityTitle}>{this.state.data.title}</Text>
+                        {/*<Text style={styles.description}> Lorem ipsum dolor sit amet,*/}
+                        {/*    consectetur adipiscing elit.*/}
+                        {/*    Vestibulum iaculis tortor odio, a*/}
+                        {/*    rutrum tortor faucibus cursus.*/}
+                        {/*    Nulla fringilla congue magna vitae*/}
+                        {/*    fermentum. Quisque eleifend arcu*/}
+                        {/*    non cursus laoreet. Duis efficitur*/}
+                        {/*    mattis eros convallis placerat.*/}
+                        {/*    Maecenas lorem dui, gravida at*/}
+                        {/*    odio eget, semper molestie leo.*/}
+                        {/*    Pellentesque rhoncus facilisis mi*/}
+                        {/*    semper ultricies.*/}
+                        {/*    Lorem ipsum dolor sit amet,*/}
+                        {/*    consectetur adipiscing elit.*/}
+                        {/*    Vestibulum iaculis tortor odio, a*/}
+                        {/*    rutrum tortor faucibus cursus.*/}
+                        {/*    Nulla fringilla congue magna vitae*/}
+                        {/*    fermentum. Quisque eleifend arcu*/}
+                        {/*    non cursus laoreet. Duis efficitur*/}
+                        {/*    mattis eros convallis placerat.*/}
+                        {/*    Maecenas lorem dui, gravida at*/}
+                        {/*    odio eget, semper molestie leo.*/}
+                        {/*    Pellentesque rhoncus facilisis mi*/}
+                        {/*    semper ultricies.</Text>*/}
+                        {/*<Text style={styles.description}>{this.state.data.description}</Text>*/}
+                        <View style={{marginBottom: 100}}/>
+                    </ScrollView>
+                </SafeAreaView>
+            </View>
+        )
+    }
 }
 
 
