793d36f0306a870639b044a6a9c5f86a4efe21b0
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-20 21:57:24 +0200

    Added new route for back to page witch contains menu on user profile.

--- routes/RoutesMyProfileInfo.js
+++ routes/RoutesMyProfileInfo.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import {Router, Scene, Stack} from "react-native-router-flux";
 import MyProfileInfo from "../screens/userProfile/MyProfileInfo";
 import ChangePassword from "../screens/ChangePassword";
+import UserProfile from "../screens/userProfile/UserProfile";
 
 export default class RoutesMyProfileInfo extends Component{
     render() {
@@ -10,6 +11,7 @@ export default class RoutesMyProfileInfo extends Component{
                 <Stack key={"root"} hideNavBar={true}>
                     <Scene key={"switchToMyProfileInfo"} component={MyProfileInfo} title={"switchToMyProfileInfo"} initial={true}/>
                     <Scene key={"switchToChangePassword"} component={ChangePassword} title={"switchToChangePassword"}/>
+                    <Scene key={"backToProfile"} component={UserProfile}/>
                 </Stack>
             </Router>
         )
