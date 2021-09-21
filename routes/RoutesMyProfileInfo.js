0ce65b2132b227d562ed3aba82aaf116a4d68baf
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-21 11:50:14 +0200

    Added new route for update profile.

--- routes/RoutesMyProfileInfo.js
+++ routes/RoutesMyProfileInfo.js
@@ -3,6 +3,9 @@ import {Router, Scene, Stack} from "react-native-router-flux";
 import MyProfileInfo from "../screens/userProfile/MyProfileInfo";
 import ChangePassword from "../screens/ChangePassword";
 import UserProfile from "../screens/userProfile/UserProfile";
+import RoutesForChallenge from "./RoutesForChallenge";
+import Leaderboard from "../screens/Leaderboard";
+import UpdateProfile from "../screens/UpdateProfile";
 
 export default class RoutesMyProfileInfo extends Component{
     render() {
@@ -12,6 +15,9 @@ export default class RoutesMyProfileInfo extends Component{
                     <Scene key={"switchToMyProfileInfo"} component={MyProfileInfo} title={"switchToMyProfileInfo"} initial={true}/>
                     <Scene key={"switchToChangePassword"} component={ChangePassword} title={"switchToChangePassword"}/>
                     <Scene key={"backToProfile"} component={UserProfile}/>
+                    <Scene key={"goToDailyChallenge"} component={RoutesForChallenge}/>
+                    <Scene key={"goToLeaderboard"} component={Leaderboard}/>
+                    <Scene key={"updateProfile"} component={UpdateProfile}/>
                 </Stack>
             </Router>
         )
