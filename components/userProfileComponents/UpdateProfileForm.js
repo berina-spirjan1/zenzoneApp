33ed5710dcbad5de61773973c4b8a9fc718a0d22
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-21 21:48:48 +0200

    Implemented form for updated user as a component and added to page. Added fetch for api.

---
+++ components/userProfileComponents/UpdateProfileForm.js
@@ -0,0 +1,342 @@
+import React, {useEffect, useState} from "react";
+import {
+    Image,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+    StyleSheet,
+    Dimensions, Platform, AsyncStorage
+} from "react-native";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import * as ImagePicker from "expo-image-picker";
+import {ACTIVITY, USER_UPDATE} from "../../configuration/config";
+import store from "../../redux/store";
+import {failedAddingActivity, startedAddingActivity, successfullyAddedActivity} from "../../redux/actions";
+
+
+export const UpdateProfileForm = () =>{
+    const screenHeight = Dimensions.get('window').height
+    let [image, setImage] = useState(null)
+    const [first_name, setFirstName] = useState('')
+    const [last_name, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [office_location, setOfficeLocation] = useState('')
+    const [work_position, setWorkPosition] = useState('')
+    const [imageUri, setImageUri] = useState('')
+    const [extension, setExtension] = useState('')
+
+    useEffect(() => {
+        (async () => {
+            if (Platform.OS !== 'web') {
+                const {status} = await ImagePicker.requestMediaLibraryPermissionsAsync();
+                if (status !== 'granted') {
+                    alert('Sorry, we need camera roll permissions to make this work!');
+                }
+            }
+        })();
+    }, []);
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [3, 3],
+            quality: 0.5
+        });
+        image = result
+        console.log(result);
+        setImageUri(Platform.OS === "android" ? image.uri : image.uri.replace("file:///", ""))
+
+        if (!result.cancelled) {
+            setImage('');
+        }
+
+    };
+
+    const updateProfile = async () => {
+
+        console.log("OVO JE FAJL", image)
+
+        let token = await AsyncStorage.getItem('jwt')
+        token = JSON.parse(token)
+        const imageType = image.type
+        setExtension(imageUri.split('.').pop())
+        const activity = new FormData();
+        activity.append('first_name',first_name);
+        activity.append('last_name',last_name);
+        activity.append('email',email);
+        activity.append('office_location', office_location);
+        activity.append('work_position', work_position);
+        activity.append('image', {
+            name: `${imageType}.${extension}`,
+            type:  `${imageType}/${extension}`,
+            uri: imageUri
+        });
+        fetch(`${USER_UPDATE}`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "multipart/form-data",
+                "Accept": "application/json",
+                'Authorization': 'Bearer ' + token
+            },
+            body: activity
+        })
+            .then(async res => {
+                try {
+                    store.dispatch(startedAddingActivity());
+
+                    const jsonRes = await res.json();
+
+                    console.log(jsonRes)
+                    if (res.status !== 200) {
+                        console.log(res.status)
+                        store.dispatch(failedAddingActivity());
+                    } else {
+                        store.dispatch(successfullyAddedActivity());
+                    }
+                } catch (err) {
+                    console.log(err);
+                }
+            })
+    }
+
+    return(
+        <View>
+            <TouchableOpacity style={styles.uploadImage}
+                              onPress={pickImage}>
+
+                {/*<Text style={styles.upload}>Upload photo</Text>*/}
+                <Image source={require('../../assets/images/user_photo.png')}
+                       style={styles.userImageDefault}/>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.littleCamera}>
+                <FontAwesome5 name={'camera'}
+                              size={20}
+                              color={'#505760'}
+                              style={styles.cameraIcon}/>
+            </TouchableOpacity>
+            <Text style={styles.username}>angel_traveler</Text>
+            <View style={styles.followersFollowing}>
+                <Text style={styles.followers}>Followers</Text>
+                <Text style={styles.following}>Following</Text>
+            </View>
+            <View style={styles.counters}>
+                <Text style={styles.counterFollowers}>0</Text>
+                <Text style={styles.following}>0</Text>
+            </View>
+            <SafeAreaView style={styles.safeArea}
+                          style={{height: screenHeight}}>
+                <ScrollView vertical={true}
+                            style={styles.scrollView}>
+                    <TouchableOpacity style={styles.items}>
+                        <View style={styles.itemRow}>
+                            <FontAwesome5 name={'signature'}
+                                          size={16}
+                                          color={'#616C75'}
+                                          style={styles.icon}/>
+                            <Text style={styles.menuItem2}>angel_traveler</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.items}>
+                        <View style={styles.itemRow}>
+                            <FontAwesome5 name={'user-circle'}
+                                          size={20}
+                                          color={'#616C75'}
+                                          style={styles.icon}/>
+                            <TextInput style={styles.menuItem}
+                                       onChangeText={setFirstName}/>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.items}>
+                        <View style={styles.itemRow}>
+                            <FontAwesome5 name={'user-circle'}
+                                          size={20}
+                                          color={'#616C75'}
+                                          style={styles.icon}/>
+                            <TextInput style={styles.menuItem}
+                                       onChangeText={setLastName}/>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.items}>
+                        <View style={styles.itemRow}>
+                            <FontAwesome5 name={'envelope'}
+                                          size={20}
+                                          color={'#616C75'}
+                                          style={styles.icon}/>
+                            <TextInput style={styles.menuItem}
+                                       onChangeText={setEmail}/>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.items}>
+                        <View style={styles.itemRow}>
+                            <FontAwesome5 name={'map-marker-alt'}
+                                          size={20}
+                                          color={'#616C75'}/>
+                            <TextInput style={styles.menuItem}
+                                       onChangeText={setOfficeLocation}/>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.items}>
+                        <View style={styles.itemRow}>
+                            <FontAwesome5 name={'briefcase'}
+                                          size={20}
+                                          color={'#616C75'}/>
+                            <TextInput style={styles.menuItem}
+                                       onChangeText={setWorkPosition}/>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button}
+                                      onPress={updateProfile}>
+                        <Text style={styles.buttonText}>Update button</Text>
+                    </TouchableOpacity>
+                    <View style={{marginBottom:50}}/>
+                </ScrollView>
+            </SafeAreaView>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    username:{
+        fontSize: 22,
+        top:-100,
+        fontFamily: 'Roboto_700Bold_Italic',
+        textAlign: 'center',
+        color:'#505760'
+    },
+    items:{
+        borderWidth: 0,
+        backgroundColor:'#CBDBF2'
+    },
+    itemRow:{
+        top: 0,
+        flexDirection: 'row',
+        paddingLeft: 80,
+        height: 60,
+
+    },
+    menuItem2:{
+        marginTop:2,
+        marginLeft: 45,
+        fontFamily: 'Roboto_700Bold_Italic',
+        fontWeight: 'bold',
+        color: '#616C75'
+    },
+    menuItem:{
+        marginTop:-40,
+        marginLeft: 45,
+        fontFamily: 'Roboto_700Bold_Italic',
+        fontWeight: 'bold',
+        color: '#616C75'
+    },
+    bottomTabBar:{
+        backgroundColor: '#93B4E5',
+        height: 60,
+        paddingLeft: 0,
+        paddingTop: 10
+    },
+    safeArea:{
+        flex:1,
+        paddingTop: StatusBar.currentHeight,
+        paddingVertical: 20
+    },
+    scrollView:{
+        alignSelf: 'stretch',
+        marginTop: -20,
+        marginBottom: 415
+    },
+    followersFollowing:{
+        flexDirection: 'row',
+        marginTop: -60,
+        marginBottom: 100
+    },
+    followers:{
+        marginLeft: 40,
+        marginRight:140,
+        fontSize: 16,
+        fontFamily: 'Roboto_700Bold_Italic',
+        fontWeight: 'bold',
+        color:'#505760'
+    },
+    following: {
+        fontSize: 16,
+        fontFamily: 'Roboto_700Bold_Italic',
+        fontWeight: 'bold',
+        color:'#505760'
+    },
+    counters:{
+        flexDirection: 'row',
+        marginTop: -100,
+        marginBottom: 75
+    },
+    counterFollowers:{
+        marginLeft: 70,
+        marginRight: 200,
+        fontSize: 16,
+        fontFamily: 'Roboto_700Bold_Italic',
+        fontWeight: 'bold',
+        color:'#505760'
+    },
+    counterFollowing:{
+        fontSize: 16,
+        fontFamily: 'Roboto_700Bold_Italic',
+        fontWeight: 'bold',
+        color:'#505760'
+    },
+    button:{
+        backgroundColor: '#93B4E5',
+        width: 150,
+        height: 50,
+        left: 170,
+        padding: 10,
+        marginBottom: 20,
+        borderRadius: 25
+    },
+    buttonText:{
+        textAlign: 'center',
+        textTransform: 'uppercase',
+        fontWeight: 'bold',
+        fontSize: 14,
+        justifyContent: 'center',
+        marginTop: 7
+    },
+    uploadImage:{
+        backgroundColor: '#93B4E5',
+        width: 100,
+        height: 100,
+        borderRadius: 100,
+        marginLeft: 130,
+        marginTop: -100,
+        marginBottom: 100
+    },
+    cameraIcon:{
+
+    },
+    upload:{
+        marginTop: 5,
+        textAlign: 'center',
+        fontWeight: 'bold'
+    },
+    userImageDefault:{
+        width: 100,
+        height: 100,
+        borderRadius: 100,
+        marginBottom: 100
+    },
+    littleCamera:{
+        backgroundColor: '#FFF',
+        height: 30,
+        width: 30,
+        borderRadius: 30,
+        marginTop: -130,
+        marginLeft: 200,
+        marginBottom: 100,
+        padding: 5
+    }
+
+})
--- screens/UpdateProfile.js
+++ screens/UpdateProfile.js
@@ -6,15 +6,18 @@ import {
     ImageBackground,
     SafeAreaView,
     ScrollView,
-    StatusBar, StyleSheet,
-    Text, TouchableOpacity,
+    StatusBar,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
     View,
     TextInput
 } from "react-native";
-import {USER} from "../configuration/config";
-import {Actions} from "react-native-router-flux";
-import {Toolbar} from "react-native-material-ui";
+import { USER } from "../configuration/config";
+import { Actions } from "react-native-router-flux";
+import { Toolbar } from "react-native-material-ui";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import {UpdateProfileForm} from "../components/userProfileComponents/UpdateProfileForm";
 
 
 export default class UpdateProfile extends Component{
@@ -55,11 +58,8 @@ export default class UpdateProfile extends Component{
         Actions.switchToChangePassword()
     }
 
-
-
     render(){
 
-        const screenHeight = Dimensions.get('window').height
 
         return(
             <View style={styles.container}>
@@ -68,94 +68,10 @@ export default class UpdateProfile extends Component{
                     backgroundColor="#334A6D"/>
                 <Toolbar style={{ container: { backgroundColor: '#93B4E5' } }}
                          leftElement="arrow-back"
-                         centerElement="My profile"/>
+                         centerElement="Update profile"/>
                 <ImageBackground source={require('../assets/images/backgroundLeaderboardLightMode.png')}
                                  style={styles.imageBackground}/>
-                <Image source={require('../assets/images/user_photo.png')}
-                       style={styles.userImage}/>
-                <TextInput style={styles.username}>{this.state.data.name}</TextInput>
-                <View style={styles.followersFollowing}>
-                    <Text style={styles.followers}>Followers</Text>
-                    <Text style={styles.following}>Following</Text>
-                </View>
-                <View style={styles.counters}>
-                    <Text style={styles.counterFollowers}>{this.state.data.followers_counter}</Text>
-                    <Text style={styles.following}>{this.state.data.following_counter}</Text>
-                </View>
-                <SafeAreaView style={styles.safeArea}
-                              style={{height: screenHeight}}>
-                    <ScrollView vertical={true}
-                                style={styles.scrollView}>
-                        <TouchableOpacity style={styles.items}>
-                            <View style={styles.itemRow}>
-                                <FontAwesome5 name={'signature'}
-                                              size={16}
-                                              color={'#616C75'}
-                                              style={styles.icon}/>
-                                <Text style={styles.menuItem}>{this.state.data.name}</Text>
-                            </View>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={styles.items}>
-                            <View style={styles.itemRow}>
-                                <FontAwesome5 name={'user-circle'}
-                                              size={20}
-                                              color={'#616C75'}
-                                              style={styles.icon}/>
-                                <TextInput style={styles.menuItem}
-                                           onChangeText={()=>this.setState({ name: this.state.data.first_name})}>{this.state.data.first_name}</TextInput>
-                            </View>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={styles.items}>
-                            <View style={styles.itemRow}>
-                                <FontAwesome5 name={'user-circle'}
-                                              size={20}
-                                              color={'#616C75'}
-                                              style={styles.icon}/>
-                                <TextInput style={styles.menuItem}
-                                           onChangeText={()=>this.setState({ name: this.state.last_name})}>{this.state.data.last_name}</TextInput>
-                            </View>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={styles.items}>
-                            <View style={styles.itemRow}>
-                                <FontAwesome5 name={'envelope'}
-                                              size={20}
-                                              color={'#616C75'}
-                                              style={styles.icon}/>
-                                <TextInput style={styles.menuItem}
-                                           onChangeText={()=>this.setState({ name: this.state.email})}>{this.state.data.email}</TextInput>
-                            </View>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={styles.items}>
-                            <View style={styles.itemRow}>
-                                <FontAwesome5 name={'map-marker-alt'}
-                                              size={20}
-                                              color={'#616C75'}/>
-                                <TextInput style={styles.menuItem}
-                                           onChangeText={()=>this.setState({ name: this.state.office_location})}>{this.state.data.office_location}</TextInput>
-                            </View>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={styles.items}>
-                            <View style={styles.itemRow}>
-                                <FontAwesome5 name={'briefcase'}
-                                              size={20}
-                                              color={'#616C75'}/>
-                                <TextInput style={styles.menuItem}
-                                           onChangeText={()=>this.setState({ name: this.state.work_position})}>{this.state.data.work_position}</TextInput>
-                            </View>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={styles.bottomTabBar}
-                                          onPress={this.switchToChangePassword}>
-                            <View style={styles.itemRow}>
-                                <FontAwesome5 name={'lock'}
-                                              size={20}
-                                              color={'#3B495E'}/>
-                                <Text style={styles.menuItem}
-                                      >Change password</Text>
-                            </View>
-                        </TouchableOpacity>
-                        <View style={{marginBottom:100}}/>
-                    </ScrollView>
-                </SafeAreaView>
+                <UpdateProfileForm/>
 
             </View>
         )
@@ -168,26 +84,26 @@ const styles = StyleSheet.create({
         flex: 1
     },
     imageBackground:{
-        height: 520,
+        height: 500,
         width: 700,
         justifyContent: 'center',
-        marginLeft:-170,
-        marginTop: -100,
-        opacity:0.2,
+        marginLeft:-150,
+        marginTop: -150,
         flex: 1,
-        marginBottom:300
+        marginBottom:300,
+        opacity:0.2
     },
     userImage:{
-        width: 150,
-        height: 150,
+        width: 100,
+        height: 100,
         borderRadius: 200,
-        top: -140,
-        left:110,
+        top: -100,
+        left:130,
         textAlign: 'center'
     },
     username:{
         fontSize: 22,
-        top:-130,
+        top:-100,
         fontFamily: 'Roboto_700Bold_Italic',
         textAlign: 'center',
         color:'#505760'
@@ -197,14 +113,21 @@ const styles = StyleSheet.create({
         backgroundColor:'#CBDBF2'
     },
     itemRow:{
-        top: 10,
+        top: 0,
         flexDirection: 'row',
         paddingLeft: 80,
         height: 60,
 
     },
-    menuItem:{
+    menuItem2:{
         marginTop:2,
+        marginLeft: 45,
+        fontFamily: 'Roboto_700Bold_Italic',
+        fontWeight: 'bold',
+        color: '#616C75'
+    },
+    menuItem:{
+        marginTop:-40,
         marginLeft: 45,
         fontFamily: 'Roboto_700Bold_Italic',
         fontWeight: 'bold',
@@ -214,7 +137,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#93B4E5',
         height: 60,
         paddingLeft: 0,
-        paddingTop:10
+        paddingTop: 10
     },
     safeArea:{
         flex:1,
@@ -223,12 +146,12 @@ const styles = StyleSheet.create({
     },
     scrollView:{
         alignSelf: 'stretch',
-        marginTop: -10,
+        marginTop: -20,
         marginBottom: 415
     },
     followersFollowing:{
         flexDirection: 'row',
-        marginTop: -110,
+        marginTop: -60,
         marginBottom: 100
     },
     followers:{
@@ -263,5 +186,56 @@ const styles = StyleSheet.create({
         fontFamily: 'Roboto_700Bold_Italic',
         fontWeight: 'bold',
         color:'#505760'
+    },
+    button:{
+        backgroundColor: '#93B4E5',
+        width: 150,
+        height: 50,
+        left: 170,
+        padding: 10,
+        marginBottom: 20,
+        borderRadius: 25
+    },
+    buttonText:{
+        textAlign: 'center',
+        textTransform: 'uppercase',
+        fontWeight: 'bold',
+        fontSize: 14,
+        justifyContent: 'center',
+        marginTop: 7
+    },
+    uploadImage:{
+        backgroundColor: '#93B4E5',
+        width: 100,
+        height: 100,
+        borderRadius: 100,
+        marginLeft: 130,
+        marginTop: -100,
+        marginBottom: 100
+    },
+    cameraIcon:{
+
+    },
+    upload:{
+        marginTop: 5,
+        textAlign: 'center',
+        fontWeight: 'bold'
+    },
+    userImageDefault:{
+        width: 100,
+        height: 100,
+        borderRadius: 100,
+        marginBottom: 100
+    },
+    littleCamera:{
+        backgroundColor: '#FFF',
+        height: 30,
+        width: 30,
+        borderRadius: 30,
+        marginTop: -130,
+        marginLeft: 200,
+        marginBottom: 100,
+        padding: 5
     }
+
 })
