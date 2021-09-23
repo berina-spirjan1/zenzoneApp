542a0748e6480062e202aba96fa520607a89781e
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-23 12:34:09 +0200

    Added new types for loading activities and categories.

--- redux/types.js
+++ redux/types.js
@@ -29,6 +29,16 @@ export const SUCCESSFULLY_ADDED_ACTIVITY = 'successfully_added_activity';
 export const FAILED_ADDING_ACTIVITY = 'failed_adding_activity';
 export const STARTED_ADDING_ACTIVITY = 'started_adding_activity';
 
+//loading categories
+export const SUCCESSFULLY_LOADED_CATEGORIES = 'successfully_loaded_categories';
+export const STARTED_LOADING_CATEGORIES = 'started_loading_categories';
+export const FAILED_LOADING_CATEGORIES = 'failed_loading_categories';
+
+//loading activities
+export const SUCCESSFULLY_LOADED_ACTIVITIES = 'successfully_loaded_activities';
+export const STARTED_LOADING_ACTIVITIES = 'started_loading_activities';
+export const FAILED_LOADING_ACTIVITIES = 'failed_loading_activities';
+
 //updating activity
 export const STARTED_UPDATING_ACTIVITY = 'started_updating_activity';
 export const FAILED_UPDATING_ACTIVITY = 'failed_updating_activity';
@@ -43,6 +53,7 @@ export const SUCCESSFULLY_DELETED_ACTIVITY = 'successfully_deleted_activity';
 export const STARTED_SENDING_REPORT_FOR_CHALLENGE = 'started_sending_report_for_challenge';
 export const FAILED_AT_SENDING_REPORT_FOR_CHALLENGE = 'failed_at_sending_report_for_challenge';
 export const SENDING_REPORT_FOR_CHALLENGE_SUCCESS = 'sending_report_for_challenge_success';
+
 
 export const STARTED_UPDATING_USER_INFORMATIONS = 'started_updating_user_informations';
 export const FAILED_UPDATING_USER_INFORMATIONS = 'failed_updating_user_informations';
