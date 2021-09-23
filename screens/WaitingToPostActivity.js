d203e51c1bca001a6b59fa765838cd64f26d1076
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-23 15:26:30 +0200

    Finished implementation for this page.

--- screens/WaitingToPostActivity.js
+++ screens/WaitingToPostActivity.js
@@ -1,15 +1,29 @@
 import React, {Component} from "react";
 import {
     View,
-    StyleSheet
+    StyleSheet,
+    StatusBar,
+    Text
 } from "react-native";
+import {Toolbar} from "react-native-material-ui";
+import LottieView from "lottie-react-native";
 
 
 export default class WaitingToPostActivity extends Component{
     render(){
         return(
-            <View>
-
+            <View style={styles.container}>
+                <StatusBar animated={true}
+                           backgroundColor="#6285B3"/>
+                <Toolbar style={{ container: { backgroundColor: '#93B4E5' }}}
+                         centerElement="All categories"/>
+                <LottieView
+                    style={styles.lottie}
+                    source={require("../assets/images/waitingToPostActivity.json")}
+                    autoPlay={true}
+                    loop={true}/>
+                <Text style={styles.text}>Please login to your account to post new activity.</Text>
+                <Text style={styles.team}>Your ZenZone team</Text>
             </View>
         )
     }
@@ -20,5 +34,26 @@ const styles = StyleSheet.create({
     container:{
         backgroundColor: '#cbdbf2',
         flex: 1
+    },
+    lottie:{
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginLeft: 20,
+        marginTop: -50
+    },
+    text:{
+        fontSize: 20,
+        padding:20,
+        marginTop: 350,
+        fontWeight: 'bold',
+        textAlign: 'right',
+        marginRight: 20
+    },
+    team:{
+        fontSize: 14,
+        marginLeft: 200,
+        fontWeight: 'bold',
+        marginTop: 20
     }
 })
