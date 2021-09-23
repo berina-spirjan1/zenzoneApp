8856e15b5b3e56acead33cbb85b0642796ae0ae0
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-23 16:09:33 +0200

    Added new route.

--- routes/RoutesHomePage.js
+++ routes/RoutesHomePage.js
@@ -5,12 +5,13 @@ import {
     Stack
 } from "react-native-router-flux";
 import HomePage from "../screens/HomePage";
-import SingleActivity from "../screens/SingleActivity";
+
 import UserProfile from "../screens/userProfile/UserProfile";
 import SideMenu from "../components/sideMenu/SideMenu";
 import Leaderboard from "../screens/Leaderboard";
 import RoutesForChallenge from "./RoutesForChallenge";
 import Settings from "../screens/userProfile/Settings";
+import AllCategories from "../screens/AllCategories";
 
 
 export default class RoutesHomePage extends Component{
@@ -19,10 +20,10 @@ export default class RoutesHomePage extends Component{
             <Router>
                 <Stack key={"root"} hideNavBar={true}>
                     <Scene key={"homePageActivities"} component={HomePage} title={"Activities"} initial={true}/>
-                    <Scene key={"singleActivity"} component={SingleActivity} title={"Single activity"}/>
+                    <Scene key={"seeAll"} component={AllCategories} title={"seeAll"}/>
                     <Scene key={"userProfile"} component={UserProfile} title={"User profile"}/>
                     <Scene key={"sideMenu"} component={SideMenu} title={"sideMenu"}/>
-                    <Scene key={"leaderboard"} component={Leaderboard} title={"leaderboard"}/>
+                    <Scene key={"leaderboard2"} component={Leaderboard} title={"leaderboard"}/>
                     <Scene key={"daily"} component={RoutesForChallenge} title={"daily"}/>
                     <Scene key={"settings"} component={Settings} title={"settings"}/>
                 </Stack>
