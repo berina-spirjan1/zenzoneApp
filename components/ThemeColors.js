0103e27763c313fc1a415881377d9a405503b98c
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-23 16:25:08 +0200

    Created component for rendering colors for light or dark theme.

---
+++ components/ThemeColors.js
@@ -0,0 +1,12 @@
+import { createTheme } from 'react-native-theming'
+
+export const themes = [
+    createTheme({
+        backgroundColor: '#CBDBF2',
+        color: '#000000',
+    }, 'light'),
+    createTheme({
+        backgroundColor: '#646668',
+        color: '#FFFFFF'
+    })
+]
