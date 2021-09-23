ebcde9ccace178d5abf8c13d84c91a5a69dc32e7
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-23 15:53:40 +0200

    Added new component to router.

--- routes/RoutesCreateActivity.js
+++ routes/RoutesCreateActivity.js
@@ -7,6 +7,7 @@ import {
 import SuccessfullyAddedActivity from "../screens/SuccessfullyAddedActivity";
 import {CreateNewActivityForm} from "../components/createNewActivityComponents/CreateNewActivityForm";
 import CreateNewActivity from "../screens/CreateNewActivity";
+import WaitingToPostActivity from "../screens/WaitingToPostActivity";
 
 
 export default class RoutesCreateActivity extends Component {
@@ -16,6 +17,7 @@ export default class RoutesCreateActivity extends Component {
                 <Stack key={"root"} hideNavBar={true}>
                     <Scene key={"switchCreateActivitySuccessfullyAdded"} component={CreateNewActivity} title={"switchCreateActivitySuccessfullyAdded"} initial={true}/>
                     <Scene key={"switchSuccessfullyAddedCreateActivity"} component={SuccessfullyAddedActivity} title={"switchSuccessfullyAddedCreateActivity"}/>
+                    <Scene key={"switchToCreateActivity"} component={WaitingToPostActivity} title={"switchToCreateActivity"}/>
                 </Stack>
             </Router>
         )
