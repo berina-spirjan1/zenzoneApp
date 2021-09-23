021d7e4ca944f9b195cb40be61cbe853e76e2119
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-23 15:07:27 +0200

    The implementation of a page for non-logged in users has started, which prevents them from posting their activity.

---
+++ screens/WaitingToPostActivity.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react";
+import {
+    View,
+    StyleSheet
+} from "react-native";
+
+
+export default class WaitingToPostActivity extends Component{
+    render(){
+        return(
+            <View>
+
+            </View>
+        )
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: '#cbdbf2',
+        flex: 1
+    }
+})
