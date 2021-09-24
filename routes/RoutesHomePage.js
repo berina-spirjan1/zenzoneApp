1469defe42b4cd34b8da494c71c9e77b32b50101
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-24 23:18:45 +0200

    Added new route for single activity to router.

--- routes/RoutesHomePage.js
+++ routes/RoutesHomePage.js
@@ -12,6 +12,7 @@ import Leaderboard from "../screens/Leaderboard";
 import RoutesForChallenge from "./RoutesForChallenge";
 import Settings from "../screens/userProfile/Settings";
 import AllCategories from "../screens/AllCategories";
+import SingleActivity from "../screens/SingleActivity";
 
 
 export default class RoutesHomePage extends Component{
@@ -20,12 +21,14 @@ export default class RoutesHomePage extends Component{
             <Router>
                 <Stack key={"root"} hideNavBar={true}>
                     <Scene key={"homePageActivities"} component={HomePage} title={"Activities"} initial={true}/>
+                    <Scene key={"singleActivity"} component={SingleActivity}/>
                     <Scene key={"seeAll"} component={AllCategories} title={"seeAll"}/>
                     <Scene key={"userProfile"} component={UserProfile} title={"User profile"}/>
                     <Scene key={"sideMenu"} component={SideMenu} title={"sideMenu"}/>
                     <Scene key={"leaderboard2"} component={Leaderboard} title={"leaderboard"}/>
                     <Scene key={"daily"} component={RoutesForChallenge} title={"daily"}/>
                     <Scene key={"settings"} component={Settings} title={"settings"}/>
+
                 </Stack>
             </Router>
         )
