c2e31ebf143e4c783efff215588716c1d5b3960c
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-24 11:34:34 +0200

    Changed component.

--- routes/RoutesForChallenge.js
+++ routes/RoutesForChallenge.js
@@ -4,6 +4,7 @@ import DailyChallengeDetails from "../screens/dailyChallenge/DailyChallengeDetai
 import DoDailyChallenge from "../screens/dailyChallenge/DoDailyChallenge";
 import Congratulations from "../screens/dailyChallenge/Congratulations";
 import DailyChallengeCounter from "../screens/dailyChallenge/DailyChallengeCounter";
+import {DoDailyChallengeForm} from "../screens/dailyChallenge/DoDailyChallengeForm";
 
 
 
@@ -14,6 +15,7 @@ export default class RoutesForChallenge extends Component{
                 <Stack key={"root"} hideNavBar={true}>
                     <Scene key={"challengeDetails"} component={DailyChallengeDetails} title={"challengeDetails"} initial={true}/>
                     <Scene key={"doDaily"} component={DoDailyChallenge} title={"doDaily"}/>
+                    <Scene key={"fromFormDaily"} component={DoDailyChallengeForm}/>
                     <Scene key={"congratulations"} component={Congratulations} title={"congratulations"}/>
                     <Scene key={"waiting"} component={DailyChallengeCounter} title={"waiting"}/>
                 </Stack>
