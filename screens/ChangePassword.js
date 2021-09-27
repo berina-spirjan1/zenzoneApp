946e0bf826592c7199ff1cd838b4396c3ff93038
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-27 11:41:12 +0200

    Added toolbar for iphone.

--- screens/ChangePassword.js
+++ screens/ChangePassword.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { StatusBar } from 'react-native';
 import { Toolbar } from "react-native-material-ui";
 import { ChangePasswordForm } from "../components/changePasswordComponents/ChangePasswordForm";
+import {isIphoneX} from "react-native-iphone-x-helper";
+import {renderIf} from "../utilities/CommonMethods";
 
 export default class ChangePassword extends Component{
     constructor(props) {
@@ -13,8 +15,10 @@ export default class ChangePassword extends Component{
             <>
                 <StatusBar animated={true}
                            backgroundColor="#6285B3"/>
-                <Toolbar style={{ container: {backgroundColor: '#93B4E5'}}}
-                         centerElement="Change password"/>
+                {renderIf(isIphoneX(), <Toolbar style={{ container: {backgroundColor: '#93B4E5', marginTop: 50}}}
+                                               centerElement="Change password"/> )}
+                {renderIf(!isIphoneX(), <Toolbar style={{ container: {backgroundColor: '#93B4E5'}}}
+                                                  centerElement="Change password"/>)}
                 <ChangePasswordForm/>
             </>
 
