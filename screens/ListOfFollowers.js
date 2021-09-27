16753221f249f79295b2893a92c9e62a26a8b69a
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-27 21:48:26 +0200

    Added different toolbar for iphone.

--- screens/ListOfFollowers.js
+++ screens/ListOfFollowers.js
@@ -6,6 +6,8 @@ import {
 import YourFamilyLightModeIcon from "../assets/icons/YourFamilyLightModeIcon";
 import FamilyCard from "../components/followerComponents/cards/FamilyCard";
 import { Toolbar } from "react-native-material-ui";
+import {renderIf} from "../utilities/CommonMethods";
+import {isIphoneX} from "react-native-iphone-x-helper";
 
 export default class ListOfFollowers extends Component{
     constructor(props) {
@@ -20,8 +22,10 @@ export default class ListOfFollowers extends Component{
             <>
                 <StatusBar animated={ true }
                            backgroundColor="#ABC4E9"/>
-                <Toolbar style={{ container: { backgroundColor: '#93b4e5' } }}
-                         centerElement="YOUR FAMILY"/>
+                {renderIf(isIphoneX(),  <Toolbar style={{ container: { backgroundColor: '#93b4e5', marginTop: 50 } }}
+                                                 centerElement="YOUR FAMILY"/>)}
+                {renderIf(!isIphoneX(), <Toolbar style={{ container: { backgroundColor: '#93b4e5' } }}
+                                                 centerElement="YOUR FAMILY"/>)}
                 <View style={styles.container}>
                     <SafeAreaView style={styles.safeArea}
                                   style={{height: screenHeight}}>
