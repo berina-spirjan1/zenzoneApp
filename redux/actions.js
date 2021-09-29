f702c9f2d2bafd189aa769df552e144a0f9ce193
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-29 10:31:22 +0200

    Implemented new actions for liking, disliking activity and loading activities and categories.

--- redux/actions.js
+++ redux/actions.js
@@ -126,3 +126,102 @@ export function successfullyAddedActivity(){
         type: actions.SUCCESSFULLY_ADDED_ACTIVITY
     }
 }
+
+//action for started loading activities
+export function startedLoadingActivities(){
+    return{
+        type: actions.STARTED_LOADING_ACTIVITIES
+    }
+}
+
+//action that means something went wrong at loading activities
+export function failedAtLoadingActivities(){
+    return{
+        type: actions.FAILED_LOADING_ACTIVITIES
+    }
+}
+
+//action for successfully loaded activities
+export function successfullyLoadedActivities(){
+    return{
+        type: actions.SUCCESSFULLY_LOADED_CATEGORIES
+    }
+}
+
+
+//action for started loading categories
+export function startedLoadingCategories(){
+    return{
+        type: actions.STARTED_LOADING_CATEGORIES
+    }
+}
+
+//action that means something went wrong at loading categories
+export function failedAtLoadingCategories(){
+    return{
+        type: actions.FAILED_LOADING_CATEGORIES
+    }
+}
+
+//action for successfully loaded activities
+export function successfullyLoadedCategories(){
+    return{
+        type: actions.SUCCESSFULLY_LOADED_CATEGORIES
+    }
+}
+
+//action for started liking activity
+export function startedLikingActivity(){
+    return{
+        type: actions.STARTED_LIKING_ACTIVITY
+    }
+}
+
+//action that means something went wrong when the user started to like activity
+export function failedAtLikingActivity(){
+    return{
+        type: actions.FAILED_LIKING_ACTIVITY
+    }
+}
+
+//action for successfully liked acitivity
+export function successfullyLikedActivity(){
+    return{
+        type: actions.SUCCESSFULLY_LIKED_ACTIVITY
+    }
+}
+
+//action that means the activity is previously liked
+export function previouslyLikedActivity(){
+    return{
+        type: actions.PREVIOUSLY_LIKED
+    }
+}
+
+//action for started disliking activity
+export function startedDislikingActivity(){
+    return{
+        type: actions.STARTED_DISLIKING_ACTIVITY
+    }
+}
+
+//action that means something went wrong when the user started to dislike activity
+export function failedAtDislikingActivity(){
+    return{
+        type: actions.FAILED_DISLIKING_ACTIVITY
+    }
+}
+
+//action for successfully disliked activity
+export function successfullyDislikedActivity(){
+    return{
+        type: actions.SUCCESSFULLY_DISLIKED_ACTIVITY
+    }
+}
+
+//action that means the activity is previously disliked
+export function previouslyDislikedActivity(){
+    return{
+        type: actions.PREVIOUSLY_DISLIKED
+    }
+}
