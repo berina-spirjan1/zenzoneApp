44f682014af1e1d0ce19b12467bea651f4ec68c9
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-09-30 22:57:21 +0200

    Implemented function to get information's about daily challenge.

--- screens/dailyChallenge/DailyChallengeDetails.js
+++ screens/dailyChallenge/DailyChallengeDetails.js
@@ -12,24 +12,30 @@ import {
 import {SafeAreaView} from "react-navigation";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 import {Actions} from "react-native-router-flux";
+import {DAILY_CHALLENGE} from "../../configuration/config";
+import {renderIf} from "../../utilities/CommonMethods";
+import DailyChallengeCounter from "./DailyChallengeCounter";
 
 export default class DailyChallengeDetails extends Component{
     constructor(props) {
         super(props);
     }
 
+    state={
+        data:[]
+    }
+
     doDaily(){
         Actions.doDaily()
     }
 
-    /*componentDidMount = () => {
-        fetch('https://neki_api.json',{
+    componentDidMount = () => {
+        fetch(`${DAILY_CHALLENGE}`,{
             method: 'GET'
         })
             .then((response) => response.json())
             .then((responseJson) => {
                 console.log(responseJson);
-
                 this.setState({
                     data: responseJson
                 })
@@ -37,58 +43,46 @@ export default class DailyChallengeDetails extends Component{
             .catch((error) => {
                 console.error(error);
             });
-    }*/
+    }
 
+    waiting(){
+        Actions.waiting()
+    }
     render(){
 
         const screenHeight = Dimensions.get('window').height
 
         return (
             <View style={stylesLightMode.container}>
-                <Image style={stylesLightMode.image}
-                       source={require('../../assets/images/img_1.png')}/>
-                <Text style={stylesLightMode.title}>daily challenge</Text>
 
-                <SafeAreaView style={stylesLightMode.safeArea}
-                              style={{height: screenHeight}}>
-                    <ScrollView vertical
-                                style={stylesLightMode.scrollView}>
-                        {/*<Text style={stylesLightMode.text}>{this.state.data}</Text>*/}
-                        <Text style={stylesLightMode.text}>
-                            Lorem ipsum dolor sit amet,
-                            consectetur adipiscing elit.
-                            Vestibulum iaculis tortor odio, a
-                            rutrum tortor faucibus cursus.
-                            Nulla fringilla congue magna vitae
-                            fermentum. Quisque eleifend arcu
-                            non cursus laoreet. Duis efficitur
-                            mattis eros convallis placerat.
-                            Maecenas lorem dui, gravida at
-                            odio eget, semper molestie leo.
-                            Pellentesque rhoncus facilisis mi
-                            semper ultricies.
-                            Lorem ipsum dolor sit amet,
-                            consectetur adipiscing elit.
-                            Vestibulum iaculis tortor odio, a
-                            rutrum tortor faucibus cursus.
-                            Nulla fringilla congue magna vitae
-                            fermentum. Quisque eleifend arcu
-                            non cursus laoreet. Duis efficitur
-                            mattis eros convallis placerat.
-                            Maecenas lorem dui, gravida at
-                            odio eget, semper molestie leo.
-                            Pellentesque rhoncus facilisis mi
-                            semper ultricies.
-                        </Text>
-                        <TouchableOpacity style={stylesLightMode.button}
-                                          onPress={this.doDaily}>
-                            <FontAwesome5 name={'check'}
-                                          size={20}
-                                          color={'#000000'}
-                                          style={stylesLightMode.buttonIcon}/>
-                        </TouchableOpacity>
-                    </ScrollView>
-                </SafeAreaView>
+                {renderIf(this.state.data.length,
+                    <View>
+                        <Image style={stylesLightMode.image}
+                               source={require('../../assets/images/img_1.png')}/>
+                        <Text style={stylesLightMode.title}>{this.state.data.title}</Text>
+
+                        <SafeAreaView style={stylesLightMode.safeArea}
+                                      style={{height: screenHeight}}>
+                            <ScrollView vertical
+                                        style={stylesLightMode.scrollView}>
+                                {/*<Text style={stylesLightMode.text}>{this.state.data}</Text>*/}
+                                <Text style={stylesLightMode.text}>
+                                    {this.state.data.description}
+                                </Text>
+                                <TouchableOpacity style={stylesLightMode.button}
+                                                  onPress={this.doDaily}>
+                                    <FontAwesome5 name={'check'}
+                                                  size={20}
+                                                  color={'#000000'}
+                                                  style={stylesLightMode.buttonIcon}/>
+                                </TouchableOpacity>
+                            </ScrollView>
+                        </SafeAreaView>
+                    </View>
+                )}
+                {renderIf(!this.state.data.length,
+                    this.waiting()
+                )}
             </View>
         )
     }
