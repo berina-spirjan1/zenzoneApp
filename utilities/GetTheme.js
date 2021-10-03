74bd725bec558e5d97eb24a6eb7fbd53d01f6d35
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-10-03 02:04:09 +0200

    Implemented function for getting theme color.

---
+++ utilities/GetTheme.js
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from "react";
+import {AsyncStorage} from "react-native";
+import  {USER} from "../configuration/config";
+
+
+export async function getTheme (){
+
+    let theme = ''
+
+    let token = await AsyncStorage.getItem('jwt')
+    token = JSON.parse(token)
+
+    fetch(`${USER}`, {
+        method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                'Authorization': 'Bearer ' + token
+            }
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                theme = responseJson.theme
+                console.log("HERE",JSON.stringify(theme))
+                theme = JSON.stringify(theme)
+                return theme
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+}
+
