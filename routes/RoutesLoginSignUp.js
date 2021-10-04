041a09ccc61abf5c2dd6ae58e4bdb3d0d1154007
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-10-04 19:15:24 +0200

    Added scene login in stack.

--- routes/RoutesLoginSignUp.js
+++ routes/RoutesLoginSignUp.js
@@ -1,22 +1,14 @@
-import React, { Component } from "react";
-import {
-    Router,
-    Stack,
-    Scene
-} from 'react-native-router-flux';
+import React, {Component} from "react";
+import {Router, Scene, Stack} from 'react-native-router-flux';
 import ForgotPassword from "../screens/ForgotPassword";
 import Login from "../screens/login/Login";
 import SignUp from "../screens/signUp/SignUp";
 import {SignUpForm} from "../components/signUpComponents/SignUpForm";
 import {LoginForm} from "../components/loginComponents/LoginForm";
-import UserProfile from "../screens/userProfile/UserProfile";
 import CheckEmail from "../screens/CheckEmail";
 import RoutesProfile from "./RoutesProfile";
 import {LoginWithLocationForm} from "../components/loginComponents/LoginWithLocationForm";
 import {SignUpWithLocationForm} from "../components/signUpComponents/SignUpWithLocationForm";
-import {onLogoutHandler} from "../components/logout/Logout";
-import LogoutSplashScreen from "../screens/LogoutSplashScreen";
-import SingleActivity from "../screens/SingleActivity";
 
 
 export default class RoutesLoginSignUp extends Component {
