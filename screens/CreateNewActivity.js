ff300ee42d61c9fd6edb267581412ade831bd7fd
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-10-04 18:00:48 +0200

    Added page that will render for user that isn't logged.

--- screens/CreateNewActivity.js
+++ screens/CreateNewActivity.js
@@ -1,29 +1,45 @@
 import React, {Component} from "react";
-import {StatusBar, StyleSheet, View} from "react-native";
+import {AsyncStorage, StatusBar, StyleSheet, View} from "react-native";
 
 import {Toolbar} from "react-native-material-ui";
 import {CreateNewActivityForm} from "../components/createNewActivityComponents/CreateNewActivityForm";
 import {renderIf} from "../utilities/CommonMethods";
 import {isIphoneX} from "react-native-iphone-x-helper";
+import WaitingToPostActivity from "./WaitingToPostActivity";
 
 export default class CreateNewActivity extends Component{
     constructor(props) {
         super();
     }
 
+    getToken = async ()=>{
+        let tokenHelper = await AsyncStorage.getItem('jwt')
+        tokenHelper = JSON.parse(tokenHelper)
+
+        return tokenHelper
+    }
+
     render() {
         return(
-            <View style={styles.container}>
-                <StatusBar
-                    animated={true}
-                    backgroundColor="#6285B3"/>
-                {renderIf(isIphoneX(),<Toolbar style={{ container: { backgroundColor: '#93B4E5', marginTop: 50 } }}
-                                               centerElement="Activities"/> )}
-                {renderIf(!isIphoneX(), <Toolbar style={{ container: { backgroundColor: '#93B4E5' } }}
-                                                        centerElement="Activities"/>)}
-                <CreateNewActivityForm/>
+            <>
+                {renderIf(this.getToken!==null,
+                    <View style={styles.container}>
+                        <StatusBar
+                            animated={true}
+                            backgroundColor="#6285B3"/>
+                        {renderIf(isIphoneX(),<Toolbar style={{ container: { backgroundColor: '#93B4E5', marginTop: 50 } }}
+                                                       centerElement="Activities"/> )}
+                        {renderIf(!isIphoneX(), <Toolbar style={{ container: { backgroundColor: '#93B4E5' } }}
+                                                         centerElement="Activities"/>)}
+                        <CreateNewActivityForm/>
 
-            </View>
+                    </View>
+                )}
+                {renderIf(this.getToken===null,
+                    <WaitingToPostActivity/>
+                )}
+            </>
+
         )
     }
 }
