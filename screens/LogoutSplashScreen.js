5145b99eaab4e3e4c6d6d16ef30cac33cf9af65b
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-10-04 11:01:36 +0200

    Added different size for pictures on iphone.

--- screens/LogoutSplashScreen.js
+++ screens/LogoutSplashScreen.js
@@ -5,20 +5,40 @@ import {
     Image,
     Text
 } from "react-native";
+import {isIphoneX} from "react-native-iphone-x-helper";
+import {renderIf} from "../utilities/CommonMethods";
 
 export default class LogoutSplashScreen extends Component{
     render() {
         return(
             <View style={styles.container}>
-                <Image source={require('../assets/images/team1.jpg')}
-                       style={styles.header}/>
-                <Image source={require('../assets/images/zendevLogo.png')}
-                       style={styles.zendevLogo}/>
-                <Text style={styles.quote}>Coming together is a beginning, staying together is progress, and working together is success.</Text>
-                <Text style={styles.seeU}>See you soon.</Text>
-                <Text style={styles.zenzoneTeam}>Your ZenZone team</Text>
-                <Image source={require('../assets/images/team2.png')}
-                       style={styles.footer}/>
+                {renderIf(!isIphoneX(),
+                    <>
+                        <Image source={require('../assets/images/team1.jpg')}
+                               style={styles.header}/>
+                        <Image source={require('../assets/images/zendevLogo.png')}
+                        style={styles.zendevLogo}/>
+                        <Text style={styles.quote}>Coming together is a beginning, staying together is progress, and working together is success.</Text>
+                        <Text style={styles.seeU}>See you soon.</Text>
+                        <Text style={styles.zenzoneTeam}>Your ZenZone team</Text>
+                        <Image source={require('../assets/images/team2.png')}
+                        style={styles.footer}/>
+                    </>
+                )}
+                {renderIf(isIphoneX(),
+                    <>
+                        <Image source={require('../assets/images/team1.jpg')}
+                               style={styles.headerIphone}/>
+                        <Image source={require('../assets/images/zendevLogo.png')}
+                               style={styles.zendevLogoIphone}/>
+                        <Text style={styles.quoteIphone}>Coming together is a beginning, staying together is progress, and working together is success.</Text>
+                        <Text style={styles.seeUIphone}>See you soon.</Text>
+                        <Text style={styles.zenzoneTeamIphone}>Your ZenZone team</Text>
+                        <Image source={require('../assets/images/team2.png')}
+                               style={styles.footerIphone}/>
+                    </>
+                )}
+
             </View>
         )
     }
@@ -56,6 +76,38 @@ const styles = StyleSheet.create({
         fontFamily: 'Roboto_100Thin_Italic'
     },
     zenzoneTeam:{
+        textAlign: 'right',
+        marginRight: 22,
+        marginBottom: 10,
+        fontFamily: 'Roboto_100Thin_Italic'
+    },
+    headerIphone:{
+        height: 300,
+        width: 380
+    },
+    zendevLogoIphone: {
+        height: 70,
+        width: 150,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 100
+    },
+    footerIphone:{
+        height: 290,
+        width: 380
+    },
+    quoteIphone:{
+        padding: 20,
+        marginTop:-10,
+        marginBottom:0,
+        fontFamily: 'Roboto_500Medium'
+    },
+    seeUIphone:{
+        textAlign: 'right',
+        marginRight: 22,
+        fontFamily: 'Roboto_100Thin_Italic'
+    },
+    zenzoneTeamIphone:{
         textAlign: 'right',
         marginRight: 22,
         marginBottom: 10,
