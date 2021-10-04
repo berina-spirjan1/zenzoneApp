b62e0138c821a6a360a9b1ab2f86b27f7e71eeab
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-10-04 18:47:48 +0200

    Optimized imports.

--- redux/store.js
+++ redux/store.js
@@ -1,5 +1,5 @@
-import { createStore } from "redux";
-import { reducer } from "./reducer";
+import {createStore} from "redux";
+import {reducer} from "./reducer";
 
 //we are holding our states in store
 const store = createStore(reducer);
