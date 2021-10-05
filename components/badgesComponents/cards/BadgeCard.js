b3735f4a7c51ca8df683c4e02ecf32e4841732e4
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-10-05 22:38:10 +0200

    Added comment.

--- components/badgesComponents/cards/BadgeCard.js
+++ components/badgesComponents/cards/BadgeCard.js
@@ -2,6 +2,8 @@ import React from "react";
 import { View } from "react-native";
 import BadgeShape from "../../../assets/icons/BadgeShape";
 
+
+//card for one badge that user receives when done daily challenge
 function BadgeCard(){
     return(
         <View>
