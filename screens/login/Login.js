00eba2557d9cdbdb640641672001cf5e08ece930
berina-spirjan1 <berina.spirjan@treca-gimnazija.edu.ba>
2021-10-07 14:57:36 +0200

    Different rending for logged users.

--- screens/login/Login.js
+++ screens/login/Login.js
@@ -2,18 +2,37 @@ import React, {Component} from 'react';
 import store from "../../redux/store";
 import {Provider} from "react-redux";
 import {LoginForm} from "../../components/loginComponents/LoginForm";
+import {AsyncStorage} from "react-native";
+import {RoutesProfileStack} from "../../routes/RoutesProfileStack";
+import {renderIf} from "../../utilities/CommonMethods";
+import UserProfile from "../userProfile/UserProfile";
 
 
 export default class Login extends Component {
     constructor(props) {
         super();
     }
+    state={
+        token: null
+    }
+
+    async componentDidMount() {
+        let tokenHelper = await AsyncStorage.getItem('jwt')
+        tokenHelper = JSON.parse(tokenHelper)
+        this.setState({token: tokenHelper})
+    }
 
     render() {
 
         return (
             <Provider store={store}>
-                <LoginForm/>
+                {console.log("----------",this.state.token)}
+                {renderIf(this.state.token===null,
+                    <LoginForm/>
+                )}
+                {renderIf(this.state.token!==null,
+                    <RoutesProfileStack/>
+                )}
             </Provider>
 
         )
